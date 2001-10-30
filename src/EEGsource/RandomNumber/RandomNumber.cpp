787dffad990897c861f2b9524d462c6a7ed16ca8
gschalk <gschalk@7ef59cdf-0624-0410-a474-e34ce222aafa>
2001-10-30 11:53:50 +0000

    .
    git-svn-id: http://www.bci2000.org/svn/trunk@9 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/EEGsource/RandomNumber/RandomNumber.cpp
+++ src/EEGsource/RandomNumber/RandomNumber.cpp
@@ -1,23 +1,25 @@
 //---------------------------------------------------------------------------
 #include <vcl.h>
 #pragma hdrstop
-USEFORM("UMain.cpp", fMain);
-USEUNIT("UStorage.cpp");
-USEUNIT("GenericADC.cpp");
-USEFORM("..\shared\UVisConfig.cpp", fVisConfig);
-USEUNIT("..\shared\UBCI2000Data.cpp");
-USEUNIT("..\shared\UBCItime.cpp");
-USEUNIT("..\shared\UBitRate.cpp");
-USEUNIT("..\shared\UCoreComm.cpp");
-USEUNIT("..\shared\UCoreMessage.cpp");
-USEUNIT("..\shared\UGenericFilter.cpp");
-USEUNIT("..\shared\UGenericSignal.cpp");
-USEUNIT("..\shared\UGenericVisualization.cpp");
-USEUNIT("..\shared\UParameter.cpp");
-USEUNIT("..\shared\UState.cpp");
-USEUNIT("..\shared\UStatus.cpp");
-USEUNIT("..\shared\USysCommand.cpp");
-USEUNIT("..\shared\BCIDirectry.cpp");
+USEUNIT("..\Application.cpp");
+USEUNIT("..\GenericADC.cpp");
+USEUNIT("..\UStorage.cpp");
+USEFORM("..\..\shared\UVisConfig.cpp", fVisConfig);
+USEUNIT("..\..\shared\UBCI2000Data.cpp");
+USEUNIT("..\..\shared\UBCItime.cpp");
+USEUNIT("..\..\shared\UBitRate.cpp");
+USEUNIT("..\..\shared\UCoreComm.cpp");
+USEUNIT("..\..\shared\UCoreMessage.cpp");
+USEUNIT("..\..\shared\UGenericFilter.cpp");
+USEUNIT("..\..\shared\UGenericSignal.cpp");
+USEUNIT("..\..\shared\UGenericVisualization.cpp");
+USEUNIT("..\..\shared\UParameter.cpp");
+USEUNIT("..\..\shared\UState.cpp");
+USEUNIT("..\..\shared\UStatus.cpp");
+USEUNIT("..\..\shared\USysCommand.cpp");
+USEUNIT("..\..\shared\BCIDirectry.cpp");
+USEUNIT("RandomNumberADC.cpp");
+USEFORM("..\UMain.cpp", fMain);
 //---------------------------------------------------------------------------
 WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
