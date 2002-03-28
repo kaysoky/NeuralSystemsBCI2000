a87eb0d1bd271b273f352e43dd01898409d91b3d
djmcfarland <djmcfarland@7ef59cdf-0624-0410-a474-e34ce222aafa>
2002-03-28 12:08:08 +0000

    .
    git-svn-id: http://www.bci2000.org/svn/trunk@35 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/Tools/BCI2000toGAB/BCI2GAB.cpp
+++ src/Tools/BCI2000toGAB/BCI2GAB.cpp
@@ -2,11 +2,7 @@
 
 #include <vcl.h>
 #pragma hdrstop
-USERES("BCI2GAB.res");
 USEFORM("UMain.cpp", fMain);
-USEUNIT("..\..\shared\V0.16\UBCI2000Data.cpp");
-USEUNIT("..\..\shared\V0.16\UParameter.cpp");
-USEUNIT("..\..\shared\V0.16\UState.cpp");
 //---------------------------------------------------------------------------
 WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
