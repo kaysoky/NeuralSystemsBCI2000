b90899773b7590107bb6751fd819ed678fd38108
gschalk <gschalk@7ef59cdf-0624-0410-a474-e34ce222aafa>
2002-06-27 10:10:52 +0000

    .
    git-svn-id: http://www.bci2000.org/svn/trunk@63 7ef59cdf-0624-0410-a474-e34ce222aafa

---
+++ src/Application/shared/UTarget.h
@@ -0,0 +1,80 @@
+//---------------------------------------------------------------------------
+
+#ifndef UTargetH
+#define UTargetH
+
+#define TARGETID_NOID           -999
+#define TARGETID_ROOT           -1
+#define TARGETID_BLANK          0
+#define TARGETID_BACKUP         1
+#define TARGETID_A              14
+#define TARGETID__              40
+#define TARGETID_ABCDEFGHI      2
+#define TARGETID_YZ_            13
+
+#define TARGETTYPE_NOTYPE       -1
+#define TARGETTYPE_WORD         1
+#define TARGETTYPE_CHARACTERS   2
+#define TARGETTYPE_CHARACTER    3
+#define TARGETTYPE_CONTROL      4
+
+#define MODE_NORMAL             1
+#define MODE_PREDICTION         2
+
+#include <syncobjs.hpp>
+
+#include "UCursor.h"
+
+class TARGET
+{
+private: 	// User declarations
+        TShape  *shape;
+        TImage  *icon;
+        TLabel  *caption;
+public:		// User declarations
+        TARGET::TARGET(int my_targetID);
+        TARGET::~TARGET();
+        TARGET  *CloneTarget();
+        TARGET  *TargetSelected(CURSOR *cursor);
+        AnsiString Caption, IconFile;
+        int     targetID, parentID;
+        int     targettype;
+        BYTE    targetposition;
+        int     Top, Left, Width, Height;
+        int     TextHeight;
+        TColor  Color, TextColor;
+        TPen    *Pen;
+        void    RenderTarget(TForm *form, TRect destrect);
+        void    HideTarget();
+        void    HighlightTarget();
+};
+
+
+class TARGETLIST
+{
+private: 	// User declarations
+        TCriticalSection        *critsec;               // critical section for screen update
+        TList   *target_list;
+public:		// User declarations
+        TARGETLIST::TARGETLIST();
+        TARGETLIST::~TARGETLIST();
+        bool    Add(TARGET *new_target);
+        int     parentID;
+        int     predictionmode;
+        TARGET  *GetTargetPtr(AnsiString caption);
+        TARGET  *GetTargetPtr(int my_targetID);
+        TARGET  *GetTargetPtr(int my_targetID, int displaypos);
+        TARGET  *TargetsSelected(CURSOR *cursor);
+        int     GetTargetID(int displaypos);
+        int     GetTargetID(AnsiString caption, int targettype);
+        int     GetMaxTargetID();
+        void    DeleteTargets();
+        void    HideTargets();
+        void    HighlightTargets();
+        void    RenderTargets(TForm *form, TRect destrect);
+        int     GetCurrentBackupPosition();
+        int     GetNumTargets();
+};
+//---------------------------------------------------------------------------
+#endif
+
