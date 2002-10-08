c917299a5724a9cfac56189a97b063119f240f6d
gschalk <gschalk@7ef59cdf-0624-0410-a474-e34ce222aafa>
2002-10-08 09:28:12 +0000

    .
    git-svn-id: http://www.bci2000.org/svn/trunk@137 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/Tools/Analysis/P3/example.m
+++ src/Tools/Analysis/P3/example.m
@@ -1,4 +1,23 @@
-% example for eric
+% example of P3 analysis
+%
+% get help for P3 function by typing 'help p3'
+%
+% In this example, the subject focused on the character 'P' (third row, fourth column of the matrix)
+% The analysis shows:
+%
+% 1) A difference between standard and oddball responses (i.e., responses to rows/columns that do or do not contain 'P')
+%    See the two averaged waveforms (at Channel 11/Cz) and their statistical difference (in r2) in Figure 1
+%
+% 2) The topographical distribution of the P300 potential (measured in r2 between standard and oddball) at 310 ms (electrode 17/CP1 bad); Figure 2
+%
+% 3) The discriminability between standard and oddball as a function of time and channels - Figure 3
+%
+% 4) The responses to the different columns at Cz (stimuli 1-6) and rows (7-12)
+%    As expected, the responses are largest for the row/column containing the 'P' 
+%    (column 4 (i.e., stimulus 4) and row 3 (i.e., stimulus 9); Figure 4
+%
+% 5) Averaged responses for each character in the matrix for channel Cz (as the average between respective row and column response)
+%
+% (C) Gerwin Schalk 10/02
 
-%[res1ch, res2ch, ressqch, stimdata] = p3('d:\BCI2000data\ww\ww016\ww2.mat', 240, 2, 650, 1, 333, 'eloc16.txt', '');
-[res1ch, res2ch, ressqch, stimdata] = p3('d:\BCI2000data\tv\tv007\tv2.mat', 240, 11, 650, 1, 300, 'eloc64.txt', '');
+[res1ch, res2ch, ressqch, stimdata] = p3('aa1.mat', 240, 11, 650, 1, 310, 'eloc64.txt', '');
