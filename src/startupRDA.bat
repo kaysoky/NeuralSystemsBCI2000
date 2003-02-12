2e56c94c7d2e80ad4c1356eb6ec95607c1210a63
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2003-02-12 02:06:52 +0000

    .
    git-svn-id: http://www.bci2000.org/svn/trunk@160 7ef59cdf-0624-0410-a474-e34ce222aafa

---
+++ src/startupRDA.bat
@@ -0,0 +1,6 @@
+start operator\operat.exe
+wait 0
+rem start EEGsource\RandomNumber\RandomNumber.exe AUTOSTART 127.0.0.1
+start EEGsource\RDAClient\RDAClient.exe AUTOSTART 127.0.0.1
+start SignalProcessing\AR\ARSignalProcessing.exe AUTOSTART 127.0.0.1
+start Application\RJB\RJB.exe AUTOSTART 127.0.0.1
