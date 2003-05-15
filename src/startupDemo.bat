26251c0563855fab859a5dfaa203dd560dc389bd
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2003-05-15 07:59:42 +0000

    .
    git-svn-id: http://www.bci2000.org/svn/trunk@200 7ef59cdf-0624-0410-a474-e34ce222aafa

---
+++ src/startupDemo.bat
@@ -0,0 +1,5 @@
+start operator\operat.exe
+wait 0
+start EEGsource\RandomNumber\RandomNumber.exe AUTOSTART 127.0.0.1
+start SignalProcessing\SWSignalProcessing\SignalProcessing.exe AUTOSTART 127.0.0.1
+start Application\LittleDemoApp\LittleDemoApp.exe AUTOSTART 127.0.0.1
