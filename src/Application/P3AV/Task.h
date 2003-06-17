709ce5d7c914f40a67e7dde37c6df314cc22d20e
hizver <hizver@7ef59cdf-0624-0410-a474-e34ce222aafa>
2003-06-17 11:14:48 +0000

    added UCoreMessage.h and UCoreComm.h references
    git-svn-id: http://www.bci2000.org/svn/trunk@233 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/Application/P3AV/Task.h
+++ src/Application/P3AV/Task.h
@@ -2,7 +2,9 @@
 #define TaskH
 
 #include "UGenericFilter.h"
-#include "UsrEnv.h";
+#include "UsrEnv.h"
+#include "UCoreComm.h"
+#include "UCoreMessage.h"
 
 /// forward declarations
 class GenericVisualization;
