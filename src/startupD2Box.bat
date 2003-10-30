31c07255a1e5b943b2390b3a252385bb5bc2803a
gschalk <gschalk@7ef59cdf-0624-0410-a474-e34ce222aafa>
2003-10-30 13:15:26 +0000

    .
    git-svn-id: http://www.bci2000.org/svn/trunk@305 7ef59cdf-0624-0410-a474-e34ce222aafa

---
+++ src/startupD2Box.bat
@@ -0,0 +1,5 @@
+start operator\operat.exe
+wait 0
+start EEGsource\RandomNumber\RandomNumber.exe AUTOSTART 127.0.0.1
+start SignalProcessing\AR\ARSignalProcessing.exe AUTOSTART 127.0.0.1
+start Application\D2Box\D2Box.exe AUTOSTART 127.0.0.1
