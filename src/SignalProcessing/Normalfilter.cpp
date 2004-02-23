34df9c66a466580b1e39063c490fe81f23e1a46d
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2004-02-23 18:33:08 +0000

    .
    git-svn-id: http://www.bci2000.org/svn/trunk@374 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/SignalProcessing/Normalfilter.cpp
+++ src/SignalProcessing/Normalfilter.cpp
@@ -12,6 +12,8 @@
 #include "UParameter.h"
 #include "UGenericVisualization.h"
 
+using namespace std;
+
 #ifdef USE_LOGFILE
 FILE *Normalfile;
 #endif // USE_LOGFILE
@@ -31,18 +33,18 @@ RegisterFilter( NormalFilter, 2.E );
 NormalFilter::NormalFilter()
 : vis( NULL )
 {
- BEGIN_PARAMETER_DEFINITIONS
-   "Filtering float UD_A=  5.0 "
-     "5.0  -100.0  100.0 // Normal Filter Up / Down Intercept",
-   "Filtering float UD_B=  5.0 "
-     "5.0  -100.0  100.0 // Normal Filter Up / Down Slope",
-   "Filtering float LR_A= -5.0 "
-     "-5.0  -100.0  100.0 // Normal Filter Left/Right Intercept",
-   "Filtering float LR_B= 5.0 "
-     "5.0  0.0  100.0 // Normal Filter Left/Right Slope",
-   "Visualize int VisualizeNormalFiltering= 1 "
-     "0 0 1 // visualize Normal filtered signals (0=no 1=yes)",
- END_PARAMETER_DEFINITIONS
+  BEGIN_PARAMETER_DEFINITIONS
+    "Filtering float UD_A=  5.0 "
+      "5.0  -100.0  100.0 // Normal Filter Up / Down Intercept",
+    "Filtering float UD_B=  5.0 "
+      "5.0  -100.0  100.0 // Normal Filter Up / Down Slope",
+    "Filtering float LR_A= -5.0 "
+      "-5.0  -100.0  100.0 // Normal Filter Left/Right Intercept",
+    "Filtering float LR_B= 5.0 "
+      "5.0  0.0  100.0 // Normal Filter Left/Right Slope",
+    "Visualize int VisualizeNormalFiltering= 1 "
+      "0 0 1 // visualize Normal filtered signals (0=no 1=yes)",
+  END_PARAMETER_DEFINITIONS
 }
 
 
@@ -128,8 +130,6 @@ void NormalFilter::Initialize()
   {
     visualize=false;
   }
-
-  return;
 }
 
 // **************************************************************************
@@ -139,35 +139,45 @@ void NormalFilter::Initialize()
 //             output - output signal for this filter
 // Returns:    N/A
 // **************************************************************************
-void NormalFilter::Process(const GenericSignal *input, GenericSignal *output)
+void NormalFilter::Process( const GenericSignal* input, GenericSignal* output )
 {
-  float   val_ud;
-  float   val_lr;
-  float   value;
-
+  const val_max = ( 1 << 15 ) - 1;
   // actually perform the Normal Filtering on the input and write it into the output signal
-  int sample= 0;
-
-  for(size_t in_channel=0; in_channel<input->Channels(); in_channel++)
+  float val_ud = ( input->GetValue( 0, 0 ) - ud_a ) * ud_b;
+  if( val_ud > val_max )
   {
-    value= input->GetValue(in_channel, sample);
-// ??
-    if( in_channel == 0 ) val_ud=  ( value - ud_a ) * ud_b;
-    if( in_channel == 1 ) val_lr=  ( value - lr_a ) * lr_b;
+    val_ud = val_max;
+    bciout << "UD value greater " << val_max << endl;
+  }
+  else if( val_ud < -val_max )
+  {
+    val_ud = -val_max;
+    bciout << "UD value below " << -val_max << endl;
   }
   output->SetValue( 0, 0, val_ud );
+
+  float val_lr = ( input->GetValue( 1, 0 ) - lr_a ) * lr_b;
+  if( val_lr > val_max )
+  {
+    val_lr = val_max;
+    bciout << "LR value greater " << val_max << endl;
+  }
+  else if( val_lr < -val_max )
+  {
+    val_lr = -val_max;
+    bciout << "LR value below " << -val_max << endl;
+  }
   output->SetValue( 1, 0, val_lr );
 
 #ifdef USE_LOGFILE
-  fprintf(Normalfile,"Process val_ud= %8.3f  val_lr= %8.3f\n",val_ud,val_lr);
+  fprintf( Normalfile, "Process val_ud= %8.3f  val_lr= %8.3f\n", val_ud, val_lr );
 #endif // USE_LOGFILE
 
   if( visualize )
   {
-    vis->SetSourceID(SOURCEID_NORMALIZER);
+    vis->SetSourceID( SOURCEID_NORMALIZER );
     vis->Send2Operator( output );
   }
-  return;
 }
 
 int NormalFilter::UpdateParameters( float new_ud_a, float new_ud_b, float new_lr_a, float new_lr_b )
