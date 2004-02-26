e62a0dcb3babd15918ea174efcee2fc11fc0238d
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2004-02-26 15:22:56 +0000

    .
    git-svn-id: http://www.bci2000.org/svn/trunk@388 7ef59cdf-0624-0410-a474-e34ce222aafa

---
+++ src/shared/MeasurementUnits.cpp
@@ -0,0 +1,73 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// File: MeasurementUnits.cpp
+//
+// Date: Feb 7, 2004
+//
+// Author: juergen.mellinger@uni-tuebingen.de
+//
+// Description: Utilities for handling measurement units and conversions.
+//
+// Changes:
+//
+//////////////////////////////////////////////////////////////////////////////
+#include "PCHIncludes.h"
+#pragma hdrstop
+
+#include "MeasurementUnits.h"
+
+#include "UBCIError.h"
+#include <cmath>
+
+using namespace std;
+
+float MeasurementUnits::sUnitsPerSec = 1.0;
+float MeasurementUnits::sUnitsPerHertz = 1.0;
+
+int
+MeasurementUnits::ToUnit( const char* inValue, const char* inUnitName, float inUnitValue )
+{
+  string value( inValue );
+  float  unit = 1.0;
+  size_t pos = value.find_first_not_of( "0123456789.Ee+- " );
+  if( pos != value.npos )
+  {
+    string unitFromValue = value.substr( pos ),
+           unitName = inUnitName;
+    if( unitFromValue.length() >= unitName.length()
+        && unitFromValue.substr( unitFromValue.length() - unitName.length() ) == unitName )
+    {
+      unit *= inUnitValue;
+      unitFromValue.erase( unitFromValue.length() - unitName.length(), unitName.length() );
+    }
+    const struct
+    {
+      const char* name;
+      float       value;
+    } prefixes[] =
+    {
+      "", 1.0,
+      "p", 1e-12,
+      "n", 1e-9,
+      "u", 1e-6, "mu", 1e-6,
+      "m", 1e-3,
+      "k", 1e3,
+      "M", 1e6,
+      "G", 1e9,
+      "T", 1e12,
+    };
+    const int numPrefixes = sizeof( prefixes ) / sizeof( *prefixes );
+    int i = 0;
+    while( i < numPrefixes && unitFromValue != prefixes[ i ].name )
+      ++i;
+    if( i < numPrefixes )
+      unit *= prefixes[ i ].value;
+    else
+      bcierr << "Unknown measurement unit in expression '" << inValue << "',"
+             << " expected '" << inUnitName << "'" << endl;
+    value.erase( pos );
+  }
+  return ::floor( unit * ::atof( value.c_str() ) + 0.5 );
+}
+
+
---
+++ src/shared/MeasurementUnits.h
@@ -0,0 +1,63 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// File: MeasurementUnits.h
+//
+// Date: Feb 7, 2004
+//
+// Author: juergen.mellinger@uni-tuebingen.de
+//
+// Description: Utilities for handling measurement units and conversions.
+//
+// Changes:
+//
+//////////////////////////////////////////////////////////////////////////////
+#ifndef MeasurementUnitsH
+#define MeasurementUnitsH
+
+#if 0
+// This class converts plain numbers to strings such as "400ms".
+class DisplayUnit
+{
+  public:
+    DisplayUnit( float inValue, const std::string& inUnitName );
+    DisplayUnit( const std::string& inUnitString );
+
+    const std::string& GetName() const               { return unitName; }
+    float              GetValue() const              { return unitValue; }
+    const std::string& WriteWithUnit( float ) const;
+
+  private:
+    float       unitValue;
+    std::string unitName;
+};
+
+inline
+std::ostream& operator<<( std::ostream& os, const DisplayUnit& du )
+{
+  du.WriteToStream( os );
+  return os;
+}
+#endif
+
+// This class converts strings such as "123.3" or "12ms" to plain numbers that
+// represent values in global units.
+class MeasurementUnits
+{
+  public:
+    static int   ReadAsTime( const char* value )
+    { return ToUnit( value, "s", sUnitsPerSec ); }
+    static void  InitializeTimeUnit( float inUnitsPerSec )
+    { sUnitsPerSec = inUnitsPerSec; }
+
+    static int   ReadAsFreq( const char* value )
+    { return ToUnit( value, "Hz", sUnitsPerHertz ); }
+    static void  InitializeFreqUnit( float inUnitsPerHertz )
+    { sUnitsPerHertz = inUnitsPerHertz; }
+
+  private:
+    static int ToUnit( const char*, const char* inUnitName, float inUnitValue );
+    static float sUnitsPerSec;
+    static float sUnitsPerHertz;
+};
+
+#endif // MeasurementUnitsH
