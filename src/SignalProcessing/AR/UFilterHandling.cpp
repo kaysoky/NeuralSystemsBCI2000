858bc8ca74bca50aa768d382f3dd5b70602408b7
gschalk <gschalk@7ef59cdf-0624-0410-a474-e34ce222aafa>
2004-10-14 07:51:50 +0000

    .
    git-svn-id: http://www.bci2000.org/svn/trunk@529 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/SignalProcessing/AR/UFilterHandling.cpp
+++ src/SignalProcessing/AR/UFilterHandling.cpp
@@ -21,6 +21,8 @@ Filter( CalibrationFilter, 2.A );
 Filter( SpatialFilter, 2.B );
 Filter( ARTemporalFilter, 2.C );
 Filter( ClassFilter, 2.D );
-Filter( StatFilter, 2.E1 );
-Filter( NormalFilter, 2.E2 );
+
+Filter( NormalFilter, 2.E1 );
+
+Filter( StatFilter, 2.E2 );
 
