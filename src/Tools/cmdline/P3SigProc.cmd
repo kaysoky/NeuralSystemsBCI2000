5e67934e57761aca72d61468f14579ceaf204fe0
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2005-01-16 15:57:42 +0000

    Initial version.
    git-svn-id: http://www.bci2000.org/svn/trunk@627 7ef59cdf-0624-0410-a474-e34ce222aafa

---
+++ src/Tools/cmdline/P3SigProc.cmd
@@ -0,0 +1,15 @@
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: File:        P3SigProc.cmd
+:: Date:        Jan 10, 2005
+:: Author:      juergen.mellinger@uni-tuebingen.de
+:: Description: A command-line script to apply the P3SignalProcessing
+::              filter chain to a BCI2000 dat file.
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+@bci_dat2stream < "%1" |(
+  TransmissionFilter   |(
+  CalibrationFilter    |(
+  SpatialFilter        |(
+  P3TemporalFilter     |(
+  ClassFilter          |(
+  NormalFilter         |(
+bci_stream2table       )))))))
