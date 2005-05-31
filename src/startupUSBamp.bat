3104648a0c51761d044bbed71198feedc8087c3e
gschalk <gschalk@7ef59cdf-0624-0410-a474-e34ce222aafa>
2005-05-31 17:33:08 +0000

    *** empty log message ***
    git-svn-id: http://www.bci2000.org/svn/trunk@777 7ef59cdf-0624-0410-a474-e34ce222aafa

---
+++ src/startupUSBamp.bat
@@ -0,0 +1,5 @@
+start operator\operat.exe
+wait 0
+start EEGsource\g.USBamp\gUSBamp.exe AUTOSTART 127.0.0.1
+start SignalProcessing\AR\ARSignalProcessing.exe AUTOSTART 127.0.0.1
+start Application\RJB\RJB.exe AUTOSTART 127.0.0.1
