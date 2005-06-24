5faec3276971fd42c358a0adff46c301407d2b0c
gschalk <gschalk@7ef59cdf-0624-0410-a474-e34ce222aafa>
2005-06-24 19:32:24 +0000

    *** empty log message ***
    git-svn-id: http://www.bci2000.org/svn/trunk@830 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/startupP3AV.bat
+++ src/startupP3AV.bat
@@ -1,7 +1,7 @@
 start operator\operat.exe
 wait 0
 start EEGsource\RandomNumber\RandomNumber.exe AUTOSTART 127.0.0.1
-start SignalProcessing\Dummy\DummySignalProcessing.exe AUTOSTART 127.0.0.1
+start SignalProcessing\P3\P3SignalProcessing.exe AUTOSTART 127.0.0.1
 cd Application\P3AV
 start P3AVTask.exe AUTOSTART 127.0.0.1
 cd ..\..
