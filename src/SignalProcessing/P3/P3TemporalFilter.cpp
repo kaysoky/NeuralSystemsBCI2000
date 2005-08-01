31d080d6076207bbce89ff26891fc3a8645e1bb4
gschalk <gschalk@7ef59cdf-0624-0410-a474-e34ce222aafa>
2005-08-01 16:55:49 +0000

    increased nr. bits for StimulusCodeRes state from 7 to 8
    git-svn-id: http://www.bci2000.org/svn/trunk@894 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/SignalProcessing/P3/P3TemporalFilter.cpp
+++ src/SignalProcessing/P3/P3TemporalFilter.cpp
@@ -50,7 +50,7 @@ P3TemporalFilter::P3TemporalFilter()
 
  BEGIN_STATE_DEFINITIONS
  // the code of the stimulus (e.g., which row/column in the P3 spelling paradigm)
-   "StimulusCodeRes 7 0 0 0",
+   "StimulusCodeRes 8 0 0 0",
  // the type of the stimulus (e.g., standard/oddball in the oddball or P3 spelling paradigm)
    "StimulusTypeRes 3 0 0 0",
  END_STATE_DEFINITIONS
