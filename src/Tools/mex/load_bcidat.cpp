7f04d3bd4299ef4c5c32e20fc4ae3e99be934088
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2006-01-18 20:21:24 +0000

    Allowed for multiple input files.
    git-svn-id: http://www.bci2000.org/svn/trunk@1014 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/Tools/mex/load_bcidat.cpp
+++ src/Tools/mex/load_bcidat.cpp
@@ -6,10 +6,13 @@
 // Description: A Matlab (mex) subroutine that reads BCI2000 .dat files into
 //  Matlab workspace variables.
 //
-//  [ signal, states, parameters ] = load_bcidat( 'filename' )
+//  [ signal, states, parameters ] = load_bcidat( 'filename1', 'filename2', ... )
 //
-//  loads signal, state, and parameter data from the file whose name is given
-//  in the function's argument.
+//  loads signal, state, and parameter data from the files whose names are given
+//  as function arguments.
+//
+//  For multiple files, number of channels, states, and signal type must be
+//  consistent.
 //
 //  The 'states' output variable will be a Matlab struct with BCI2000 state
 //  names as struct member names, and the number of state value entries matching
@@ -19,8 +22,14 @@
 //  parameter names as struct member names.
 //  Individual parameters are represented as cell arrays of strings, and may
 //  be converted into numeric matrices by Matlab's str2double function.
+//  If multiple files are given, parameter values will match the first files'
+//  parameters.
+//
 //
 // $Log$
+// Revision 1.3  2006/01/18 20:21:24  mellinger
+// Allowed for multiple input files.
+//
 // Revision 1.2  2006/01/17 17:39:44  mellinger
 // Fixed list of project files.
 //
@@ -41,16 +50,29 @@ typedef signed short int16;
 typedef signed int   int32;
 typedef float        float32;
 
+typedef vector<BCI2000DATA*> FileContainer;
+
 template<typename T>
 void
-ReadSignal( BCI2000DATA& inFile, mxArray* inSignal )
+ReadSignal( FileContainer& inFiles, mxArray* ioSignal )
 {
-  T* data = reinterpret_cast<T*>( mxGetData( inSignal ) );
-  int numChannels = inFile.GetNumChannels(),
-      numSamples = inFile.GetNumSamples();
-  for( int sample = 0; sample < numSamples; ++sample )
-    for( int channel = 0; channel < numChannels; ++channel )
-      data[ numSamples * channel + sample ] = inFile.ReadValue( channel, sample );
+  T* data = reinterpret_cast<T*>( mxGetData( ioSignal ) );
+  int sampleOffset = 0,
+      totalSamples = 0;
+  for( FileContainer::iterator i = inFiles.begin(); i != inFiles.end(); ++i )
+    totalSamples += ( *i )->GetNumSamples();
+
+  for( FileContainer::iterator i = inFiles.begin(); i != inFiles.end(); ++i )
+  {
+    BCI2000DATA* file = *i;
+    int numSamples = file->GetNumSamples(),
+        numChannels = file->GetNumChannels();
+    for( int sample = 0; sample < numSamples; ++sample )
+      for( int channel = 0; channel < numChannels; ++channel )
+        data[ totalSamples * channel + sample + sampleOffset ]
+          = file->ReadValue( channel, sample );
+    sampleOffset += numSamples;
+  }
 }
 
 void
@@ -64,76 +86,111 @@ mexFunction( int nargout, mxArray* varargout[],
 
   if( nargin < 1 )
     mexErrMsgTxt( "No file name given." );
-  if( nargin > 1 )
-    mexWarnMsgTxt( "Ignoring extra input arguments." );
-  char* fileName = mxArrayToString( varargin[ 0 ] );
-  if( fileName == NULL )
-    mexErrMsgTxt( "Out of memory when reading file name." );
 
-  // Open the file.
-  BCI2000DATA file;
-  int result = file.Initialize( fileName );
-  if( result == BCI2000ERR_FILENOTFOUND )
-    result = file.Initialize( ( string( fileName ) + ".dat" ).c_str() );
-  switch( result )
+  // Open the files.
+  FileContainer files;
+  for( int i = 0; i < nargin; ++i )
   {
-    case BCI2000ERR_NOERR:
-      break;
+    files.push_back( new BCI2000DATA );
+    char* fileName = mxArrayToString( varargin[ i ] );
+    if( fileName == NULL )
+      mexErrMsgTxt( "Out of memory when reading file name." );
+    int result = files[ i ]->Initialize( fileName );
+    if( result == BCI2000ERR_FILENOTFOUND )
+      result = files[ i ]->Initialize( ( string( fileName ) + ".dat" ).c_str() );
+    switch( result )
+    {
+      case BCI2000ERR_NOERR:
+        break;
 
-    case BCI2000ERR_FILENOTFOUND:
-      {
-        ostringstream oss;
-        oss << "File \"" << fileName << "\" does not exist.";
-        mexErrMsgTxt( oss.str().c_str() );
-      }
-      break;
+      case BCI2000ERR_FILENOTFOUND:
+        {
+          ostringstream oss;
+          oss << "File \"" << fileName << "\" does not exist.";
+          mexErrMsgTxt( oss.str().c_str() );
+        }
+        break;
 
-    default:
-      {
-        ostringstream oss;
-        oss << "Could not open \"" << fileName << "\" as a BCI2000 data file.";
-        mexErrMsgTxt( oss.str().c_str() );
-      }
+      default:
+        {
+          ostringstream oss;
+          oss << "Could not open \"" << fileName << "\" as a BCI2000 data file.";
+          mexErrMsgTxt( oss.str().c_str() );
+        }
+    }
+    mxFree( fileName );
+    __bcierr.clear();
   }
-  mxFree( fileName );
-  __bcierr.clear();
 
-  // Read EEG data into the first output argument.
-  mxArray* signal = NULL;
-  int dim[] = { file.GetNumSamples(), file.GetNumChannels() };
-  switch( file.GetSignalType() )
+  int totalSamples = files[ 0 ]->GetNumSamples(),
+      numChannels = files[ 0 ]->GetNumChannels();
+  SignalType dataType = files[ 0 ]->GetSignalType();
+  mxClassID classID = mxUNKNOWN_CLASS;
+  switch( dataType )
   {
     case SignalType::int16:
-      signal = mxCreateNumericArray( 2, dim, mxINT16_CLASS, mxREAL );
-      if( signal != NULL )
-        ReadSignal<int16>( file, signal );
+      classID = mxINT16_CLASS;
       break;
-
     case SignalType::int32:
-      signal = mxCreateNumericArray( 2, dim, mxINT32_CLASS, mxREAL );
-      if( signal != NULL )
-        ReadSignal<int32>( file, signal );
+      classID = mxINT32_CLASS;
+      break;
+    case SignalType::float32:
+      classID = mxSINGLE_CLASS;
+      break;
+    default:
+      classID = mxDOUBLE_CLASS;
+  }
+
+  // Assess whether the files are compatible.
+  const STATELIST* statelist = files[ 0 ]->GetStateListPtr();
+  for( size_t i = 1; i < files.size(); ++i )
+  {
+    totalSamples += files[ i ]->GetNumSamples();
+    if( files[ i ]->GetNumChannels() != numChannels )
+      mexErrMsgTxt( "All input files must have identical numbers of channels." );
+
+    if( files[ i ]->GetSignalType() != dataType )
+      classID = mxDOUBLE_CLASS;
+
+    if( nargout > 1 ) // The caller wants us to read state information.
+      for( size_t j = 0; j < statelist->Size(); ++j )
+        if( !files[ i ]->GetStateListPtr()->Exists( ( *statelist )[ j ].GetName() ) )
+          mexErrMsgTxt( "Incompatible state information in input files." );
+  }
+
+  // Read EEG data into the first output argument.
+  int dim[] = { totalSamples, numChannels };
+  mxArray* signal = mxCreateNumericArray( 2, dim, classID, mxREAL );
+  if( signal == NULL )
+    mexErrMsgTxt( "Out of memory when allocating space for the signal variable." );
+  switch( classID )
+  {
+    case mxINT16_CLASS:
+      ReadSignal<int16>( files, signal );
       break;
 
-    case SignalType::float32:
-      signal = mxCreateNumericArray( 2, dim, mxSINGLE_CLASS, mxREAL );
-      if( signal != NULL )
-        ReadSignal<float32>( file, signal );
+    case mxINT32_CLASS:
+      ReadSignal<int32>( files, signal );
+      break;
+
+    case mxSINGLE_CLASS:
+      ReadSignal<float32>( files, signal );
+      break;
+
+    case mxDOUBLE_CLASS:
+      ReadSignal<double>( files, signal );
       break;
 
     default:
-      mexErrMsgTxt( "Unknown signal type" );
+      mexErrMsgTxt( "Unsupported class ID" );
   }
-  if( signal == NULL )
-    mexErrMsgTxt( "Out of memory when allocating space for the signal variable." );
-
   __bcierr.clear();
   varargout[ 0 ] = signal;
 
   // Read state data if appropriate.
   if( nargout > 1 )
   {
-    const STATELIST* statelist = file.GetStateListPtr();
+    const STATELIST* statelist = files[ 0 ]->GetStateListPtr();
     int numStates = statelist->Size();
     char** stateNames = new char*[ numStates ];
     StateRef** stateRefs = new StateRef*[ numStates ];
@@ -143,10 +200,6 @@ mexFunction( int nargout, mxArray* varargout[],
       const STATE& s = ( *statelist )[ i ];
       stateNames[ i ] = new char[ strlen( s.GetName() ) + 1 ];
       strcpy( stateNames[ i ], s.GetName() );
-      stateRefs[ i ] = new StateRef(
-        const_cast<STATEVECTOR*>( file.GetStateVectorPtr() ),
-        s.GetLocation(), s.GetLength()
-      );
     }
     mxArray* states = mxCreateStructMatrix(
       1, 1, numStates, const_cast<const char**>( stateNames )
@@ -154,24 +207,36 @@ mexFunction( int nargout, mxArray* varargout[],
     for( int i = 0; i < numStates; ++i )
     {
       mxArray* stateArray = mxCreateNumericMatrix(
-        file.GetNumSamples(), 1, mxINT16_CLASS, mxREAL
+        totalSamples, 1, mxINT16_CLASS, mxREAL
       );
       if( stateArray == NULL )
         mexErrMsgTxt( "Out of memory when allocating space for state variables." );
       mxSetFieldByNumber( states, 0, i, stateArray );
       stateData[ i ] = reinterpret_cast<int16*>( mxGetData( stateArray ) );
     }
-    for( size_t sample = 0; sample < file.GetNumSamples(); ++sample )
-    {
-      file.ReadStateVector( sample );
+    for( FileContainer::iterator file = files.begin(); file != files.end(); ++file )
+    { // StateRefs are not necessarily compatible across files, so we must
+      // create StateRefs for each file individually.
       for( int i = 0; i < numStates; ++i )
-        *stateData[ i ]++ = ( *stateRefs[ i ] );
+      {
+        const STATE& s = ( *statelist )[ stateNames[ i ] ];
+        stateRefs[ i ] = new StateRef(
+          const_cast<STATEVECTOR*>( ( *file )->GetStateVectorPtr() ),
+          s.GetLocation(), s.GetLength()
+        );
+      }
+      for( size_t sample = 0; sample < ( *file )->GetNumSamples(); ++sample )
+      { // Iterating over samples in the outer loop will avoid scanning
+        // the file multiple times.
+        ( *file )->ReadStateVector( sample );
+        for( int i = 0; i < numStates; ++i )
+          *stateData[ i ]++ = ( *stateRefs[ i ] );
+      }
+      for( int i = 0; i < numStates; ++i )
+        delete stateRefs[ i ];
     }
     for( int i = 0; i < numStates; ++i )
-    {
       delete[] stateNames[ i ];
-      delete stateRefs[ i ];
-    }
     delete[] stateNames;
     delete[] stateRefs;
     delete[] stateData;
@@ -183,7 +248,7 @@ mexFunction( int nargout, mxArray* varargout[],
   // Read parameters if appropriate.
   if( nargout > 2 )
   {
-    const PARAMLIST* paramlist = file.GetParamListPtr();
+    const PARAMLIST* paramlist = files[ 0 ]->GetParamListPtr();
     int numParams = paramlist->Size();
     char** paramNames = new char*[ numParams ];
     for( int i = 0; i < numParams; ++i )
@@ -213,4 +278,6 @@ mexFunction( int nargout, mxArray* varargout[],
     __bcierr.clear();
     varargout[ 2 ] = params;
   }
+  for( FileContainer::iterator i = files.begin(); i != files.end(); ++i )
+    delete *i;
 }
--- src/Tools/mex/load_bcidat.m
+++ src/Tools/mex/load_bcidat.m
@@ -1,9 +1,13 @@
+[ signal, states, parameters ] = load_bcidat( varargin )
 %LOAD_BCIDAT Load a BCI2000 data file into Matlab workspace variables.
 %
-%  [ signal, states, parameters ] = load_bcidat( 'filename' )
+%  [ signal, states, parameters ] = load_bcidat( 'filename1', 'filename2', ... )
 %
-%  loads signal, state, and parameter data from the file whose name is given
-%  in the function's argument.
+%  loads signal, state, and parameter data from the files whose names are given
+%  as function arguments.
+%
+%  For multiple files, number of channels, states, and signal type must be
+%  consistent.
 %
 %  The 'states' output variable will be a Matlab struct with BCI2000 state
 %  names as struct member names, and the number of state value entries matching
@@ -13,6 +17,8 @@
 %  parameter names as struct member names.
 %  Individual parameters are represented as cell arrays of strings, and may
 %  be converted into numeric matrices by Matlab's str2double function.
+%  If multiple files are given, parameter values will match the first files'
+%  parameters.
 %
 %  The load_bcidat function is part of the BCI2000 project 
 %  (http://www.bciresearch.org).
@@ -21,6 +27,10 @@
 %  load_bcimat.mex.
 %  $Id$
 %  $Log$
+%  Revision 1.2  2006/01/18 20:21:24  mellinger
+%  Allowed for multiple input files.
+%
 %  Revision 1.1  2006/01/17 17:15:47  mellinger
 %  Initial version.
 %
+error( 'There is no load_bcidat mex file for your platform available.' );
