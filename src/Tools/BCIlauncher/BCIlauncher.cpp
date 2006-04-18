716f04e063c73a670995dc0197dcd4fbfce828c3
awilson <awilson@7ef59cdf-0624-0410-a474-e34ce222aafa>
2006-04-18 14:10:55 +0000

    Program files added to CVS
    git-svn-id: http://www.bci2000.org/svn/trunk@1091 7ef59cdf-0624-0410-a474-e34ce222aafa

---
+++ src/Tools/BCIlauncher/BCIlauncher.cpp
@@ -0,0 +1,35 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+//---------------------------------------------------------------------------
+USEFORM("Unit1.cpp", mainForm);
+//---------------------------------------------------------------------------
+WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+    try
+    {
+         Application->Initialize();
+         Application->Title = "BCI Launcher";
+         Application->HelpFile = "";
+         Application->CreateForm(__classid(TmainForm), &mainForm);
+         Application->Run();
+    }
+    catch (Exception &exception)
+    {
+         Application->ShowException(&exception);
+    }
+    catch (...)
+    {
+         try
+         {
+             throw Exception("");
+         }
+         catch (Exception &exception)
+         {
+             Application->ShowException(&exception);
+         }
+    }
+    return 0;
+}
+//---------------------------------------------------------------------------
