02c8bddbdb3a140e94b2c4e7f190de018a45437f
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2007-02-20 19:08:03 +0000

    Added copyright notes to source files.
    git-svn-id: http://www.bci2000.org/svn/trunk@1262 7ef59cdf-0624-0410-a474-e34ce222aafa

---
+++ src/EEGsource/Biosemi_old_UCSD_implementation/BufferReadADC.h
@@ -0,0 +1,48 @@
+/* (C) 2000-2007, BCI2000 Project
+/* http://www.bci2000.org
+/*/
+#ifndef BufferReadADCH
+#define BufferReadADCH
+
+#include "GenericADC.h"
+
+class BufferReadADC : public GenericADC
+{
+ public:
+               BufferReadADC();
+  virtual      ~BufferReadADC();
+
+  virtual void Preflight( const SignalProperties&, SignalProperties& ) const;
+  virtual void Initialize();
+  virtual void Process( const GenericSignal*, GenericSignal* );
+  virtual void Halt();
+
+//  short  sineminamplitude, sinemaxamplitude;
+//  short  noiseminamplitude, noisemaxamplitude;
+//  float  sinefrequency;
+//  short  DCoffset;
+//  size_t sinechannel;
+//  bool   modulateamplitude;
+
+ private:
+//  int ADConfig();
+
+ private:
+  int   samplerate;
+  int   blocksize;
+  int   channels;
+  int   SleepTime;
+/*
+  UINT  ChanType;
+  UINT  ListSize;
+  DBL   dGain;
+  int   ClkSource;
+  DBL   dfFreq;
+  UINT  Bufferpts;
+*/
+  int   StartFlag;
+
+};
+
+#endif // BufferReadADCH
+
