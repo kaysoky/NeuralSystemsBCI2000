d79e40d2c5d4d40841bd53d75d9c420d2481a648
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2008-02-15 16:26:00 +0000

    Updated CursorTask reference data file to current version (comparison test failed due to spurious rounding differences in CursorPosition).
    git-svn-id: http://www.bci2000.org/svn/trunk@1790 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/buildutils/testing/do_compare.bat
+++ src/buildutils/testing/do_compare.bat
@@ -14,7 +14,7 @@ PATH="..\..\..\tools\cmdline";%PATH%
 for /r "%1" %%i in (*.dat) do bci_dat2stream --transmit-ds --raw < "%%i" | bci_stream2asc | grep -v+ "SourceTime|StimulusTime|State \{" > "%%i.tmp~"
 for /r "%2" %%i in (*.dat) do bci_dat2stream --transmit-ds --raw < "%%i" | bci_stream2asc | grep -v+ "SourceTime|StimulusTime|State \{" > "%%i.tmp~"
 PATH=%OLDPATH%
-( for /d %%i in ("%1\*") do fc /W /LB1 "%1\%%~ni\*.tmp~" "%2\%%~ni\*.tmp~" || goto found_diffs ) >nul
+for /d %%i in ("%1\*") do fc /W /LB1 "%1\%%~ni\*.tmp~" "%2\%%~ni\*.tmp~" || goto found_diffs
 goto end
 
 :found_diffs
