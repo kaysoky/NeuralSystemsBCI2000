0f4a42e237bfd2aa8478b600a75d091fac805765
jfialkoff <jfialkoff@7ef59cdf-0624-0410-a474-e34ce222aafa>
2008-04-09 01:05:37 +0000

    Functionality is obsolete.
    git-svn-id: http://www.bci2000.org/svn/trunk@1907 7ef59cdf-0624-0410-a474-e34ce222aafa

--- tools/OfflineAnalysis/regexprepCompat.m
+++
@@ -1,7 +0,0 @@
-function s = regexprepCompat(string, expression, replace)
-
-if strncmp(version, '6', 1)
-  s = regexprep(string, expression, replace, 'tokenize');
-else
-  s = regexprep(string, expression, replace);
-end
