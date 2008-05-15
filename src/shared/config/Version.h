a4accb2814d72eae746c65a8f6bdbb4bf223f7ae
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2008-05-15 14:55:49 +0000

    Undoing accidental commit of version header.
    git-svn-id: http://www.bci2000.org/svn/trunk@1964 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/shared/config/Version.h
+++ src/shared/config/Version.h
@@ -1,19 +1,18 @@
 ////////////////////////////////////////////////////////////////////////////////
 // $Id$
 // Author: juergen.mellinger@uni-tuebingen.de
-// Description: Version.h template to be processed using TortoiseSVN's SubWCRev
-//   tool.
+// Description: Default Version.h file. During a "make build", this file will
+//   be replaced with more accurate information.
 //
-// (C) 2000-2007, BCI2000 Project
+// (C) 2000-2008, BCI2000 Project
 // http://www.bci2000.org
 ////////////////////////////////////////////////////////////////////////////////
 #ifndef VERSION_H
 #define VERSION_H
 
-#define BCI2000_VERSION  "$Version: 2.0 $ " \
-                         "$Revision$ " \
-                         "$Build Date: Thu May 15 16:47:04 MEST 2008 $ "
-                         
+#define BCI2000_VERSION  "$Version: 2.x $ " \
+                         "$Build Date: " __DATE__ " $"
+
 #define BCI2000_COPYRIGHT "(C) 2000-2008, BCI2000 Project\n" \
                           "http://www.bci2000.org"
 
