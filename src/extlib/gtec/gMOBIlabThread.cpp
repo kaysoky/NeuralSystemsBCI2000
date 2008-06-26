34051243d386f5a6c3726d2cf2c0317e5282d1fa
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2008-06-26 18:52:58 +0000

    Improved detection of broken connection.
    git-svn-id: http://www.bci2000.org/svn/trunk@2035 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/extlib/gtec/gMOBIlabThread.cpp
+++ src/extlib/gtec/gMOBIlabThread.cpp
@@ -18,6 +18,7 @@ using namespace std;
 const int cBufBlocks = 32;        // Size of data ring buffer in terms of sample blocks.
 const int cMaxReadBuf = 1024;     // Restriction of gtec driver.
 const int cMinDataTimeout = 1000; // Minimum data timeout in ms.
+const int cMaxVoidLoopCount = 3;  // Maximum number of zero reads in data acquisition loop.
 
 gMOBIlabThread::gMOBIlabThread( int inBlockSize, int inTimeout, HANDLE inDevice )
 : OSThread( true ),
@@ -44,7 +45,7 @@ gMOBIlabThread::~gMOBIlabThread()
 sint16
 gMOBIlabThread::ExtractData()
 {
-  while( mReadCursor == mWriteCursor && !this->IsTerminated() )
+  while( mReadCursor == mWriteCursor && mEvent != NULL )
     ::WaitForSingleObject( mEvent, mTimeout );
   sint16 value = *reinterpret_cast<sint16*>( mpBuffer + mReadCursor );
   mReadCursor += sizeof( sint16 );
@@ -55,18 +56,19 @@ gMOBIlabThread::ExtractData()
 int
 gMOBIlabThread::Execute()
 {
-  enum { ok = 0, errorOccurred = 1 };
+  enum { ok = 0, error = 1 } result = ok;
 
   OVERLAPPED ov;
   ::memset( &ov, 0, sizeof( ov ) );
   ov.hEvent = mEvent;
   ov.Offset = 0;
   ov.OffsetHigh = 0;
-             
-  while( !this->IsTerminating() )
+
+  while( !this->IsTerminating() && result == ok )
   {
+    int voidLoopCount = 0;
     DWORD bytesReceived = 0;
-    while( mWriteCursor < mBufSize )
+    while( mWriteCursor < mBufSize && result == ok )
     {
       _BUFFER_ST buf;
       buf.pBuffer = reinterpret_cast<SHORT*>( mpBuffer + mWriteCursor );
@@ -74,19 +76,26 @@ gMOBIlabThread::Execute()
       buf.size = min( buf.size, cMaxReadBuf );
       buf.validPoints = 0;
       if( !::GT_GetData( mDev, &buf, &ov ) )
-        return errorOccurred;
+        result = error;
       int dataTimeout = max( cMinDataTimeout, 2 * mTimeout );
       if( WAIT_OBJECT_0 != ::WaitForSingleObject( mEvent, dataTimeout ) )
-        return errorOccurred;
+        result = error;
       if( !::GetOverlappedResult( mDev, &ov, &bytesReceived, FALSE ) )
-        return errorOccurred;
+        result = error;
+      if( bytesReceived == 0 && ++voidLoopCount > cMaxVoidLoopCount )
+        result = error;
       mWriteCursor += bytesReceived;
     }
     if( mWriteCursor > mBufSize )
-      return errorOccurred;
+      result = error;
     mWriteCursor = 0;
   }
-  return ok;
+  if( result != ok )
+  {
+    ::CloseHandle( mEvent );
+    mEvent = NULL;
+  }
+  return result;
 }
 
 
