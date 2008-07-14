a32d1750fe79b6ee12a7a802279fadcdc05f50ed
roostenveld <roostenveld@7ef59cdf-0624-0410-a474-e34ce222aafa>
2008-07-14 08:57:16 +0000

    added source code as it was used during the workshop
    git-svn-id: http://www.bci2000.org/svn/trunk@2050 7ef59cdf-0624-0410-a474-e34ce222aafa

---
+++ src/contrib/SignalProcessing/FieldTripFilter/FieldTripBufferFilter.h
@@ -0,0 +1,67 @@
+////////////////////////////////////////////////////////////////////////////////
+// $Id: FieldTripBufferFilter.h,v 1.4 2008/06/20 08:34:38 jurmel Exp $
+// Author: juergen.mellinger@uni-tuebingen.de
+// Description: A filter that maintains a FieldTrip Realtime buffer, writes
+//   its input data into that buffer, and optionally gets its output data from
+//   buffered events. BCI2000 state variables are mapped to buffer events.
+//
+// (C) 2000-2008, BCI2000 Project
+// http://www.bci2000.org
+////////////////////////////////////////////////////////////////////////////////
+#ifndef FIELDTRIP_BUFFER_FILTER_H
+#define FIELDTRIP_BUFFER_FILTER_H
+
+#include "GenericFilter.h"
+
+#include "buffer.h"
+#include <pthread.h>
+#include <set>
+#include <map>
+#include <string>
+
+class FieldTripBufferFilter : public GenericFilter
+{
+ public:
+          FieldTripBufferFilter();
+  virtual ~FieldTripBufferFilter();
+  virtual void Preflight( const SignalProperties&, SignalProperties& ) const;
+  virtual void Initialize( const SignalProperties&, const SignalProperties& );
+  virtual void StartRun();
+  virtual void StopRun();
+  virtual void Process( const GenericSignal& Input, GenericSignal& Output );
+
+ private:
+  typedef std::set<std::string> StateSet;
+  typedef std::map<std::string, int> StateValueMap;
+  void ConfigureStateSets( StateSet& ioToBufferSet, StateSet& ioFromBufferSet ) const;
+  void ProcessEvent( const event_t& );
+  void PutStateToBuffer( const std::string& );
+  template<typename T> void EventToSignal( const event_t&, GenericSignal& ) const;
+  template<typename T> message_t* SendRequest( int, T*, void* = NULL ) const;
+  message_t* SendRequest( int type ) const
+             { return SendRequest<messagedef_t>( type, NULL ); }
+  void DeleteMessage( message_t* ) const;
+  void ParseHostAddress( const std::string&, host_t& ) const;
+
+  std::string    mFTBufferAddress;
+
+  GenericSignal  mSignalBuffer;
+  std::string    mFTOutputEventType;
+  StateSet       mFTStatesToBuffer,
+                 mFTStatesFromBuffer;
+  StateValueMap  mPreviousStateValues;
+
+  unsigned long  mBlockCount;
+  host_t         mHostAddress;
+  float*         mpDataBuffer;
+  UINT8_T*       mpEventBuffer;
+  datadef_t      mDatadef;
+  headerdef_t    mHeaderdef;
+
+  pthread_attr_t mThreadAttr;
+  pthread_t      mServerThread;
+};
+
+#endif // FIELDTRIP_BUFFER_FILTER_H
+
+
---
+++ src/contrib/SignalProcessing/FieldTripFilter/PipeDefinition.cpp
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////////////
+// $Id: PipeDefinition.cpp,v 1.2 2008/06/19 08:55:24 jurmel Exp $
+// Description: This file defines which filters will be used, and the sequence
+//   in which they are applied.
+//   Each Filter() entry consists of the name of the filter and a string which,
+//   by lexical comparison, defines the relative position of the filter in the
+//   sequence.
+//
+// (C) 2000-2008, BCI2000 Project
+// http://www.bci2000.org
+////////////////////////////////////////////////////////////////////////////////
+#include "PCHIncludes.h"
+#pragma hdrstop
+
+#include "FieldTripBufferFilter.h"
+#include "ExpressionFilter.h"
+
+Filter( FieldTripBufferFilter, 2.B );
+Filter( ExpressionFilter, 2.C );
