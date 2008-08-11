kaysoky/NeuralSystemsBCI2000

commit 1342331c942e04999f8fa68167cbfc26c1e47c56

Some minor performance-related changes to BCI2000FileReader.
git-svn-id: http://www.bci2000.org/svn/trunk@2067 7ef59cdf-0624-0410-a474-e34ce222aafa

src/shared/fileio/dat/BCI2000FileReader.cpp
@@ -60,7 +60,6 @@ ReadValue_SwapBytes( const char* p )
 BCI2000FileReader::BCI2000FileReader()
 : mpStatevector( NULL ),
   mpBuffer( NULL ),
-  mfpReadValueBinary( NULL ),
   mErrorState( NoError )
 {
 }
@@ -68,7 +67,6 @@ BCI2000FileReader::BCI2000FileReader()
 BCI2000FileReader::BCI2000FileReader( const char* inFileName )
 : mpStatevector( NULL ),
   mpBuffer( NULL ),
-  mfpReadValueBinary( NULL ),
   mErrorState( NoError )
 {
   Open( inFileName );
@@ -116,10 +114,13 @@ BCI2000FileReader::Reset()
   mHeaderLength = 0;
   mStatevectorLength = 0;
   mSamplingRate = 1;
-  mDataSize = 2;
-  mfpReadValueBinary = ReadValue<sint16>;
+  mSignalType = SignalType::int16;
+  mDataSize = mSignalType.Size();
+  mSourceOffsets.clear();
+  mSourceGains.clear();
   mNumSamples = 0;
-  mSignalProperties = ::SignalProperties( 0, 0, SignalType::int16 );
+  mSignalProperties = ::SignalProperties( 0, 0, mSignalType );
+
   mErrorState = NoError;
 }
 
@@ -199,7 +200,42 @@ BCI2000FileReader::State( const std::string& name ) const
 GenericSignal::ValueType
 BCI2000FileReader::RawValue( int inChannel, long inSample )
 {
-  return mfpReadValueBinary( BufferSample( inSample ) + mDataSize * inChannel );
+  GenericSignal::ValueType value = 0;
+  const char* address = BufferSample( inSample ) + mDataSize * inChannel;
+
+  // When running on a big endian machine, we need to swap bytes.
+  static const bool isBigEndian = ( *reinterpret_cast<const uint16*>( "\0\1" ) == 0x0001 );
+  if( isBigEndian )
+  {
+    switch( mSignalType )
+    {
+      case SignalType::int16:
+        value = ReadValue_SwapBytes<sint16>( address );
+        break;
+      case SignalType::int32:
+        value = ReadValue_SwapBytes<sint32>( address );
+        break;
+      case SignalType::float32:
+        value = ReadValue_SwapBytes<float32>( address );
+        break;
+    }
+  }
+  else
+  { // Little endian machine
+    switch( mSignalType )
+    {
+      case SignalType::int16:
+        value = ReadValue<sint16>( address );
+        break;
+      case SignalType::int32:
+        value = ReadValue<sint32>( address );
+        break;
+      case SignalType::float32:
+        value = ReadValue<float32>( address );
+        break;
+    }
+  }
+  return value;
 }
 
 // **************************************************************************
@@ -275,38 +311,7 @@ BCI2000FileReader::ReadHeader()
       return;
   }
   mDataSize = signalType.Size();
-  // When running on a big endian machine, we need to swap bytes.
-  bool isBigEndian = ( *reinterpret_cast<const uint16*>( "\0\1" ) == 0x0001 );
-  if( isBigEndian )
-  {
-    switch( signalType )
-    {
-      case SignalType::int16:
-        mfpReadValueBinary = ReadValue_SwapBytes<sint16>;
-        break;
-      case SignalType::int32:
-        mfpReadValueBinary = ReadValue_SwapBytes<sint32>;
-        break;
-      case SignalType::float32:
-        mfpReadValueBinary = ReadValue_SwapBytes<float32>;
-        break;
-    }
-  }
-  else
-  { // Little endian machine
-    switch( signalType )
-    {
-      case SignalType::int16:
-        mfpReadValueBinary = ReadValue<sint16>;
-        break;
-      case SignalType::int32:
-        mfpReadValueBinary = ReadValue<sint32>;
-        break;
-      case SignalType::float32:
-        mfpReadValueBinary = ReadValue<float32>;
-        break;
-    }
-  }
+
   // now go through the header and read all parameters and states
   getline( mFile >> ws, line, '\n' );
   if( line.find( "[ State Vector Definition ]" ) != 0 )

src/shared/fileio/dat/BCI2000FileReader.h
@@ -26,7 +26,7 @@
 class BCI2000FileReader
 {
  public:
-  static const int cDefaultBufSize = 50000;
+  static const int cDefaultBufSize = 50 * 1024;
 
   enum
   {
@@ -110,15 +110,15 @@ class BCI2000FileReader
                      mFileFormatVersion;
 
   ::SignalProperties mSignalProperties;
+  SignalType         mSignalType;
   int                mDataSize;
-  GenericSignal::ValueType ( *mfpReadValueBinary )( const char* );
 
   int                mChannels,
                      mHeaderLength,
                      mStatevectorLength;
   float              mSamplingRate;
-  std::vector<float> mSourceOffsets,
-                     mSourceGains;
+  std::vector<GenericSignal::ValueType> mSourceOffsets,
+                                        mSourceGains;
 
   unsigned long      mNumSamples;
 

commit 15adcabd3af5b1f2992fe749f1416424699a1157

Fixed a bug introduced in r2067.
git-svn-id: http://www.bci2000.org/svn/trunk@2081 7ef59cdf-0624-0410-a474-e34ce222aafa

src/shared/fileio/dat/BCI2000FileReader.cpp
@@ -302,15 +302,15 @@ BCI2000FileReader::ReadHeader()
   if( element != "StatevectorLen=" )
     return;
 
-  SignalType signalType = SignalType::int16;
+  mSignalType = SignalType::int16;
   if( linestream >> element )
   {
     if( element != "DataFormat=" )
       return;
-    if( !( linestream >> signalType ) )
+    if( !( linestream >> mSignalType ) )
       return;
   }
-  mDataSize = signalType.Size();
+  mDataSize = mSignalType.Size();
 
   // now go through the header and read all parameters and states
   getline( mFile >> ws, line, '\n' );
@@ -333,7 +333,7 @@ BCI2000FileReader::ReadHeader()
   int sampleBlockSize = 1;
   if( mParamlist.Exists( "SampleBlockSize" ) )
     sampleBlockSize = ::atoi( mParamlist[ "SampleBlockSize" ].Value().c_str() );
-  mSignalProperties = ::SignalProperties( mChannels, sampleBlockSize, signalType );
+  mSignalProperties = ::SignalProperties( mChannels, sampleBlockSize, mSignalType );
 
   const float defaultOffset = 0.0;
   mSourceOffsets.clear();