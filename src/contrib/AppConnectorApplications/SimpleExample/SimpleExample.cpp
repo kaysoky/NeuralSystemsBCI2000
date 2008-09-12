c582200427942104a3305b29b223a8d640f17b04
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2008-09-12 13:06:02 +0000

    Extended SimpleExample connector demo to allow writing as well.
    git-svn-id: http://www.bci2000.org/svn/trunk@2129 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/contrib/AppConnectorApplications/SimpleExample/SimpleExample.cpp
+++ src/contrib/AppConnectorApplications/SimpleExample/SimpleExample.cpp
@@ -1,8 +1,10 @@
 ////////////////////////////////////////////////////////////////////////////////
 // $Id$
 // Author: juergen.mellinger@uni-tuebingen.de
-// Description: An example that illustrates how to read data from an UDP port.
-//   Data are written to stdout.
+// Description: An example that illustrates how to read/write data from an UDP
+//   port.
+//   Data read from the UDP port are written to stdout; data written into the
+//   UDP port are read from stdin.
 //
 // (C) 2000-2008, BCI2000 Project
 // http://www.bci2000.org
@@ -16,18 +18,43 @@ using namespace std;
 int
 main( int argc, char* argv[] )
 {
+  bool write = false;
+  if( argc > 2 )
+  {
+    string arg = string( argv[1] );
+    if( arg == "-w" )
+      write = true;
+    else if( arg == "-r" )
+      write = false;
+  }
+
   const char* address = "localhost:20320";
   if( argc > 1 )
-    address = argv[1];
+    address = argv[argc-1];
 
-  receiving_udpsocket socket( address );
-  sockstream input( socket );
-  if( !input.is_open() )
-    cerr << "Could not open " << address
-         << "." << endl;
+  if( write )
+  { // Read from stdin, write to socket.
+    sending_udpsocket socket( address );
+    sockstream output( socket );
+    if( !output.is_open() )
+      cerr << "Could not open " << address
+           << " for output." << endl;
 
-  string line;
-  while( getline( input, line ) )
-    cout << line << endl;
+    string line;
+    while( getline( cin, line ) )
+      output << line << endl;
+  }
+  else
+  { // Read from socket, write to stdout.
+    receiving_udpsocket socket( address );
+    sockstream input( socket );
+    if( !input.is_open() )
+      cerr << "Could not open " << address
+           << " for input." << endl;
+
+    string line;
+    while( getline( input, line ) )
+      cout << line << endl;
+  }
   return 0;
 }
