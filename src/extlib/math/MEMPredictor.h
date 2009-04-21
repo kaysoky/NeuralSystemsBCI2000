e8002dcf80e82367ef8347b80fe1d1479f1a8f29
awilson <awilson@7ef59cdf-0624-0410-a474-e34ce222aafa>
2009-04-21 22:13:37 +0000

    reverted yet again to figure out compilation problems
    git-svn-id: http://www.bci2000.org/svn/trunk@2341 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/extlib/math/MEMPredictor.h
+++ src/extlib/math/MEMPredictor.h
@@ -1,10 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 // $Id$
 // Authors: mcfarlan@wadsworth.org, juergen.mellinger@uni-tuebingen.de
-// Update: 3/26/09 jawilson@cae.wisc.edu - A more efficient method for finding
-//      the AR coefficients was implemented from:
-//      N. Andersen. Comments on the performance of maximum entropy algorithms. 
-//          Proceedings of the IEEE, 66(11):1581?1582, Nov. 1978.
 // Description: This LinearPredictor implements the Maximum Entropy Method for
 //     autoregressive spectral analysis adapted from Press et. al.
 //     Numerical Recipes in C (chapter 13).
@@ -18,8 +14,7 @@
 #include "LinearPredictor.h"
 #include <numeric>
 #include <limits>
-#include <math>
- 
+
 template<typename T>
 class MEMPredictor : public LinearPredictor<T>
 {
@@ -53,7 +48,7 @@ MEMPredictor<T>::TransferFunction( const DataVector& inData ) const
 {
   typedef double D;
   const T eps = std::numeric_limits<T>::epsilon();
-  DataVector  coeff, wkm,
+  static DataVector  coeff, wkm,
                      wk1, wk2;
   int n = inData.size();
   coeff.resize( LinearPredictor<T>::mModelOrder + 1 );
@@ -61,57 +56,43 @@ MEMPredictor<T>::TransferFunction( const DataVector& inData ) const
   wk1.resize( n );
   wk1 = inData;
   wk2.resize( n );
-  wk2 = inData;
+  wk2 = inData.shift( 1 );
 
-  D meanPower = 0;
-  D den = 0;
-  for (int t = 0; t < n; t++)
-    meanPower += (wk1[t]*wk1[t]);
-  
-  den = meanPower*2;
-  meanPower /= n;
-  D num=0.0;
-  D q = 1.0;
+  D meanPower = std::inner_product( &wk1[0], &wk1[n], &wk1[0], D( 0.0 ) ) / n;
   coeff[0] = 1.0;
   for( int k = 1; k <= LinearPredictor<T>::mModelOrder; ++k )
   {
-    num = 0;
-    for (int t = 0; t < n-k; t++)
-        num += wk1[t+1]*wk2[t];
-    
-    den = den*q - wk1[0]*wk1[0] - wk2[n-k]*wk2[n-k];
-   
-    coeff[k] = 2*num / den;
-    if (fabs(coeff[k]) >= 1){
-        den = 0;
-        for (int t = 0; t < n-k; t++)
-            den += wk1[t+1]*wk1[t+1] + wk2[t]*wk2[t];
-        
-        coeff[k] = 2*num/den;
+    D num   = ( n > k ) ?
+              2.0 * std::inner_product( &wk1[0], &wk1[n-k], &wk2[0], D( 0.0 ) ) :
+              0.0,
+      denom = ( n > k ) ?
+              std::inner_product( &wk1[0], &wk1[n-k], &wk1[0], D( 0.0 ) )
+              + std::inner_product( &wk2[0], &wk2[n-k], &wk2[0], D( 0.0 ) ) :
+              0.0;
+    if( denom < eps )
+    { // limit for zero data
+      num = 1.0;
+      denom = 1.0;
     }
-    q = 1.0 - coeff[k] * coeff[k];
-    meanPower *= q;
+    coeff[k] = - num / denom;
+    meanPower *= 1.0 - coeff[k] * coeff[k];
     for( int i = 1; i < k; ++i )
-      coeff[i] = wkm[i] - coeff[k] * wkm[k-i];
+      coeff[i] = wkm[i] + coeff[k] * wkm[k-i];
 
     if( k < LinearPredictor<T>::mModelOrder )
     {
       for( int i = 1; i <= k; ++i )
         wkm[i] = coeff[i];
 
-      for( int j = 0; j < n-k; ++j )
+      for( int j = 0; j < n-k-1; ++j )
       {
-        wk1[j] = wk1[j+1] - wkm[k] * wk2[j];
-        wk2[j] = wk2[j] - wkm[k] * wk1[j+1];
+        wk1[j] += wkm[k] * wk2[j];
+        wk2[j] = wk2[j+1] + wkm[k] * wk1[j+1];
       }
     }
   }
   if( meanPower < 0.0 )
     meanPower = 0.0;
-  
-  for (int k = 1; k <= LinearPredictor<T>::mModelOrder; k++)
-      coeff[k] *= -1;
- 
   static Ratpoly<Complex> result;
   result = Ratpoly<Complex>(
              Polynomial<Complex>( std::sqrt( meanPower ) ),
