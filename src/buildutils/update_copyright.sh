44942726d60add11d5e1273ce39ea10a2e5651b6
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2009-05-07 11:22:57 +0000

    Fixed compiler warnings in ExpressionParser.cpp.
    git-svn-id: http://www.bci2000.org/svn/trunk@2347 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/buildutils/update_copyright.sh
+++ src/buildutils/update_copyright.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for i in `find . -iname "*.cpp"` `find . -iname "*.h"` `find . -iname "*.m"` `find . -iname "Makefile"` `find . -iname "*.bat"`;
+for i in `find . -iname "*.cpp"` `find . -iname "*.h"` `find . -iname "*.m"` `find . -iname "Makefile"` `find . -iname "*.bat"` `find . -iname "*.y"`;
   do echo "$i";
   mv "$i" "$i.bak" && ( sed -b 's/(C) 2000-2008, BCI2000 Project/(C) 2000-2009, BCI2000 Project/' < "$i.bak" > "$i" )
 done;
