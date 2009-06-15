fd09c4c401bb2099806fdde23c706cd3229f6bee
jhill <jhill@7ef59cdf-0624-0410-a474-e34ce222aafa>
2009-06-15 07:44:38 +0000

    added OSX support in PrecisionTime, based on mach_absolute_time (tested, but have not rigourously assessed precision)
    git-svn-id: http://www.bci2000.org/svn/trunk@2401 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/shared/utils/PrecisionTime.cpp
+++ src/shared/utils/PrecisionTime.cpp
@@ -11,39 +11,65 @@
 
 #include "PrecisionTime.h"
 
-#ifdef _WIN32
-# include <windows.h>
-#else
-# include <time.h>
-#endif
-
 // **************************************************************************
 // Function:   Now()
 // Purpose:    gets the current time from the system's high-performance timers
 // Parameters:
 // Returns:    an unsigned sixteen bit value for the current time in ms
-//             (-> wrap-around occurs every 65536ms, or about 6.5 seconds)
+//             (-> wrap-around occurs every 65536ms, or about 65 seconds)
 // **************************************************************************
+#if defined ( _WIN32 )
+// **************************************************************************
+
+#include <windows.h>
 PrecisionTime
 PrecisionTime::Now()
 {
-#ifdef _WIN32
-
   // Get the current time from the Windows precision timer.
   LARGE_INTEGER prectime, prectimebase;
   ::QueryPerformanceCounter( &prectime );
   ::QueryPerformanceFrequency( &prectimebase );
   return ( prectime.QuadPart * 1000 ) / prectimebase.QuadPart;
+}
 
-#else // _WIN32
+// **************************************************************************
+#elif defined ( __APPLE__ )
+// **************************************************************************
 
-  // Use clock_gettime() on non-Windows systems.
+#include <mach/mach_time.h>
+PrecisionTime
+PrecisionTime::Now()
+{
+  static int64_t mt0;
+  static double  multiplier = 0.0;  
+  if(!multiplier) {
+  	mach_timebase_info_data_t mtbinfo;
+    mach_timebase_info( &mtbinfo );
+    multiplier = 1.0e-6 * (double(mtbinfo.numer) / double(mtbinfo.denom));
+    mt0 = mach_absolute_time();
+  }
+  return multiplier * double(mach_absolute_time() - mt0);
+}
+
+// **************************************************************************
+#else // neither _WIN32 nor __APPLE__
+// **************************************************************************
+
+#include <time.h>
+PrecisionTime
+PrecisionTime::Now()
+{
+  // Use clock_gettime() on non-Windows systems with librt.
   struct timespec t;
   ::clock_gettime( CLOCK_REALTIME, &t );
   return ( t.tv_sec * 1000 ) + t.tv_nsec / 1000000;
-
-#endif // _WIN32
 }
+
+// **************************************************************************
+#endif // _WIN32, __APPLE__
+// **************************************************************************
+
+
 
 // **************************************************************************
 // Function:   TimeDiff
