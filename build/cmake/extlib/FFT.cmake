d3b5487980ca628865ba5540b79342555cf55b58
jhill <jhill@7ef59cdf-0624-0410-a474-e34ce222aafa>
2011-04-04 23:50:26 +0000

    ExpressionFilter added to command-line tools CMakeLists FFT.cmake now issues a warning, and sets FFT_OK to FALSE, if libfftw3.a is not found, at least on non-win32 platforms ( any reason why not to do this before/on windows? )
    git-svn-id: http://www.bci2000.org/svn/trunk@3171 7ef59cdf-0624-0410-a474-e34ce222aafa

--- build/cmake/extlib/FFT.cmake
+++ build/cmake/extlib/FFT.cmake
@@ -8,6 +8,9 @@
 ##       INC_EXTLIB_FFT - Include directory for the fft library
 ##       Also defines source groups for the fft files
 
+# Set success
+SET( FFT_OK TRUE )
+
 # Define the source files
 SET( SRC_EXTLIB_FFT
   ${BCI2000_SRC_DIR}/extlib/fftlib/FFTLibWrap.cpp
@@ -20,6 +23,7 @@ SET( HDR_EXTLIB_FFT
 
 # Define the include directory
 SET( INC_EXTLIB_FFT ${BCI2000_SRC_DIR}/extlib/fftlib )
+
 IF( APPLE )
   SET( INC_EXTLIB_FFT
     ${INC_EXTLIB_FFT}
@@ -34,11 +38,16 @@ ENDIF( APPLE )
 # Set Libs required
 IF( NOT WIN32 )
   SET( LIBS_EXTLIB_FFT libfftw3.a )
+  
+  FIND_LIBRARY( FFTW3_LIBRARY     NAMES fftw3     PATHS ${LIBDIR_EXTLIB_FFT} )
+  IF( ${FFTW3_LIBRARY}   STREQUAL   "FFTW3_LIBRARY-NOTFOUND" )
+    MESSAGE( "- WARNING: failed to find FFTW3 library" )
+    SET( FFT_OK   FALSE )
+  ENDIF( ${FFTW3_LIBRARY}  STREQUAL  "FFTW3_LIBRARY-NOTFOUND" )
+  
 ENDIF( NOT WIN32 )
+
 
 # Set the source groups
 SOURCE_GROUP( Source\\BCI2000_Framework\\extlib\\fftlib FILES ${SRC_EXTLIB_FFT} )
 SOURCE_GROUP( Headers\\BCI2000_Framework\\extlib\\fftlib FILES ${HDR_EXTLIB_FFT} )
-
-# Set success
-SET( FFT_OK TRUE )
