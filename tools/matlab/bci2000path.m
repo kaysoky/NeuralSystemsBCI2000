b51c47e5633d62bf4b63d34022253b42a3a00b04
jhill <jhill@7ef59cdf-0624-0410-a474-e34ce222aafa>
2011-05-07 02:10:30 +0000

    added license, authorship and svn keywords to matlab tools
    git-svn-id: http://www.bci2000.org/svn/trunk@3268 7ef59cdf-0624-0410-a474-e34ce222aafa

--- tools/matlab/bci2000path.m
+++ tools/matlab/bci2000path.m
@@ -34,6 +34,32 @@ function [out, isd, isf] = bci2000path(varargin)
 %      % Remove BCI2000 toolboxes:
 %      BCI2000PATH -RemoveAll
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% $Id$ 
+%% Author: Jeremy Hill <jezhill@gmail.com>
+%%
+%% $BEGIN_BCI2000_LICENSE$
+%% 
+%% This file is part of BCI2000, a platform for real-time bio-signal research.
+%% [ Copyright (C) 2000-2011: BCI2000 team and many external contributors ]
+%% 
+%% BCI2000 is free software: you can redistribute it and/or modify it under the
+%% terms of the GNU General Public License as published by the Free Software
+%% Foundation, either version 3 of the License, or (at your option) any later
+%% version.
+%% 
+%% BCI2000 is distributed in the hope that it will be useful, but
+%%                         WITHOUT ANY WARRANTY
+%% - without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+%% A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+%% 
+%% You should have received a copy of the GNU General Public License along with
+%% this program.  If not, see <http://www.gnu.org/licenses/>.
+%% 
+%% $END_BCI2000_LICENSE$
+%% http://www.bci2000.org 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 global BCI2000ROOT
 
 cmd = '';
