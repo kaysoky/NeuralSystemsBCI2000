d9cdf3b24a0a6f1ceb4b29a2589d9878ae8dfc68
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2011-05-12 15:35:30 +0000

    Replaced the "prefix hack" that was previously used to direct output files into a single directory (not "Release" or "Debug" subdirectories) with the official approach of setting a number of CMake variables/target properties. AFAIK, this works only from CMake 2.8.3, so this is now the required version for building BCI2000. There is now a macro SET_OUTPUT_DIRECTORY( dir [target1 target2 ...] ) which sets the variables when no target is given, and the properties when targets are present.
    git-svn-id: http://www.bci2000.org/svn/trunk@3282 7ef59cdf-0624-0410-a474-e34ce222aafa

---
+++ build/cmake/SetOutputDirectoryMacro.cmake
@@ -0,0 +1,40 @@
+###########################################################################
+## $Id$
+## Authors: juergen.mellinger@uni-tuebingen.de
+## Description: Contains macros to set CMake output directories.
+##   SET_OUTPUT_DIRECTORY( dir [target1, target2, ...] )
+##   When no targets are given, CMAKE_..._OUTPUT_DIRECTORY... variables
+##   are set; when targets are given, ..._OUTPUT_DIRECTORY... properties
+##   are set on the targets.
+
+MACRO( SET_OUTPUT_DIRECTORY )
+
+  SET( targets ${ARGN} )
+  LIST( GET targets 0 dir ) # LIST( GET ${ARGN} 0 dir ) fails, returning NOTFOUND in dir
+  LIST( REMOVE_AT targets 0 )
+  
+  SET( products Runtime Library Archive )
+  SET( configs Debug Release RelWithDebInfo MinSizeRel )
+  
+  FOREACH( product ${products} )
+    STRING( TOUPPER "${product}" product )
+
+    IF( targets )
+      SET_TARGET_PROPERTIES( ${targets} PROPERTIES "${product}_OUTPUT_DIRECTORY" "${dir}" )
+    ELSE()
+      SET( "CMAKE_${product}_OUTPUT_DIRECTORY" "${dir}" )
+    ENDIF()
+    
+    FOREACH( config ${configs} )
+      STRING( TOUPPER "${config}" config )
+
+      IF( targets )
+        SET_TARGET_PROPERTIES( ${targets} PROPERTIES "${product}_OUTPUT_DIRECTORY_${config}" "${dir}" )
+      ELSE()
+        SET( "CMAKE_${product}_OUTPUT_DIRECTORY_${config}" "${dir}" )
+      ENDIF()
+      
+    ENDFOREACH()
+  ENDFOREACH()
+  
+ENDMACRO()
