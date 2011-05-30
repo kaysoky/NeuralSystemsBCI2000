cbb93b35c153525bec0c9a1aaec4ca2c9ab95454
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2011-05-30 16:51:37 +0000

    Fixed a bug in computation of timeout value.
    git-svn-id: http://www.bci2000.org/svn/trunk@3294 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/core/SignalSource/g.USBamp/gUSBampADC.cpp
+++ src/core/SignalSource/g.USBamp/gUSBampADC.cpp
@@ -4,23 +4,23 @@
 // Description: BCI2000 Source Module for gUSBamp devices.
 //
 // $BEGIN_BCI2000_LICENSE$
-// 
+//
 // This file is part of BCI2000, a platform for real-time bio-signal research.
 // [ Copyright (C) 2000-2011: BCI2000 team and many external contributors ]
-// 
+//
 // BCI2000 is free software: you can redistribute it and/or modify it under the
 // terms of the GNU General Public License as published by the Free Software
 // Foundation, either version 3 of the License, or (at your option) any later
 // version.
-// 
+//
 // BCI2000 is distributed in the hope that it will be useful, but
 //                         WITHOUT ANY WARRANTY
 // - without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 // A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 // $END_BCI2000_LICENSE$
 ////////////////////////////////////////////////////////////////////////////////
 #include "PCHIncludes.h"
@@ -482,7 +482,7 @@ void gUSBampADC::Initialize(const SignalProperties&, const SignalProperties&)
     // thus, we set the timeout to be small, e.g., 1.5 times the size of
     // one sample block and simply give a warning
     // this is not perfect but I simply can't do it better at the moment
-    m_timeoutms=(int)(MeasurementUnits::SampleBlockDuration())*1000*5;
+    m_timeoutms=static_cast<int>(MeasurementUnits::SampleBlockDuration()*1000*5);
 
     // determine the number of devices and allocate the buffers accordingly
     m_numdevices=Parameter("DeviceIDs")->NumValues();
