8a28f3059740b0a0c1b463b26562815b9e08caa7
jhill <jhill@7ef59cdf-0624-0410-a474-e34ce222aafa>
2011-06-21 02:23:20 +0000

    fleshed out audiostream batch file
    git-svn-id: http://www.bci2000.org/svn/trunk@3338 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/contrib/BCPy2000/framework/BCPy2000/private/audiostream/dummy.bat
+++ src/contrib/BCPy2000/framework/BCPy2000/private/audiostream/dummy.bat
@@ -5,7 +5,18 @@
 cd ..\prog
 call portable.bat
 
-start operat.exe                      --OnConnect "-LOAD PARAMETERFILE %WD%\parms\audiostream_soundcard_250Hz.prm ; LOAD PARAMETERFILE %WD%\parms\audiostream_EEG67+EOG3+SYNC2+VMRK_Quickamp_500Hz.prm ; SETCONFIG"
-start PythonSource.exe                --PythonSrcWD=%WD%\python
-start PythonSignalProcessing.exe      --PythonSigWD=%WD%\python
-start PythonApplication.exe           --PythonAppWD=%WD%\python
+
+@set OnConnect=-
+
+@set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %WD%\parms\audiostream_EEG67+EOG3+SYNC2+VMRK_Quickamp_500Hz.prm 
+@set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %WD%\parms\explore_pulses.prm 
+@set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %WD%\parms\headphones001.prm
+@set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %WD%\subject_attention.prm 
+::@set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %WD%\subject_perception.prm 
+
+@set OnConnect=%OnConnect% ; SETCONFIG
+
+start operat.exe                      --OnConnect "%OnConnect%"
+start PythonSource.exe                --PythonSrcWD=%WD%\python --PythonSrcShell=1 --PythonSrcLog=%WD%\log\###-src.txt
+start PythonSignalProcessing.exe      --PythonSigWD=%WD%\python --PythonSigShell=1 --PythonSigLog=%WD%\log\###-sig.txt
+start /REALTIME PythonApplication.exe --PythonAppWD=%WD%\python --PythonAppShell=1 --PythonAppLog=%WD%\log\###-app.txt
