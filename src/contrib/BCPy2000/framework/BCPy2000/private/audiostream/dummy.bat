549f22caa3c98bd4b22dc7ad52924e5848f40059
jhill <jhill@7ef59cdf-0624-0410-a474-e34ce222aafa>
2011-06-22 00:18:44 +0000

    initial experimental version of 32/64-channel usbamp prm file for audiostream. other tweaks to enable said experimentation
    git-svn-id: http://www.bci2000.org/svn/trunk@3342 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/contrib/BCPy2000/framework/BCPy2000/private/audiostream/dummy.bat
+++ src/contrib/BCPy2000/framework/BCPy2000/private/audiostream/dummy.bat
@@ -9,14 +9,20 @@ call portable.bat
 @set OnConnect=-
 
 @set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %WD%\parms\audiostream_EEG67+EOG3+SYNC2+VMRK_Quickamp_500Hz.prm 
+@set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %WD%\parms\gUSBamps.prm 
 @set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %WD%\parms\explore_pulses.prm 
-@set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %WD%\parms\headphones001.prm
+::@set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %WD%\parms\headphones001.prm
 @set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %WD%\subject_attention.prm 
 ::@set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %WD%\subject_perception.prm 
 
 @set OnConnect=%OnConnect% ; SETCONFIG
 
-start operat.exe                      --OnConnect "%OnConnect%"
-start PythonSource.exe                --PythonSrcWD=%WD%\python --PythonSrcShell=1 --PythonSrcLog=%WD%\log\###-src.txt
-start PythonSignalProcessing.exe      --PythonSigWD=%WD%\python --PythonSigShell=1 --PythonSigLog=%WD%\log\###-sig.txt
-start /REALTIME PythonApplication.exe --PythonAppWD=%WD%\python --PythonAppShell=1 --PythonAppLog=%WD%\log\###-app.txt
+start           operat                   --OnConnect "%OnConnect%"
+start           gUSBampSource
+::start           PythonSource             --PythonSrcWD=%WD%\python --PythonSrcShell=1 --PythonSrcLog=%WD%\log\###-src.txt
+
+::start           DummySignalProcessing
+start           PythonSignalProcessing   --PythonSigWD=%WD%\python --PythonSigShell=1 --PythonSigLog=%WD%\log\###-sig.txt --PythonSigClassFile=
+
+::start           DummyApplication
+start /REALTIME PythonApplication        --PythonAppWD=%WD%\python --PythonAppShell=1 --PythonAppLog=%WD%\log\###-app.txt --PythonAppClassFile=
