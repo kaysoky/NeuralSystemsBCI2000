5e09049700cb50a44503a6c42c1f210bc26a5b59
jhill <jhill@7ef59cdf-0624-0410-a474-e34ce222aafa>
2011-07-01 23:53:05 +0000

    16+2 channel setup for audiostream
    git-svn-id: http://www.bci2000.org/svn/trunk@3374 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/contrib/BCPy2000/framework/BCPy2000/private/audiostream/audiostream.bat
+++ src/contrib/BCPy2000/framework/BCPy2000/private/audiostream/audiostream.bat
@@ -9,8 +9,13 @@
 @set PROG=%PYTHONHOME%\..\..\BCI2000\prog
 @if exist %PROG% cd %PROG%
 @if exist %PROG% goto start
-
 :start
+
+
+@set SESSION=001
+@if [%1]==[] goto SKIPSESSIONARG
+@set SESSION=%1
+:SKIPSESSIONARG
 
 cd ..\prog
 call portable.bat
@@ -18,10 +23,15 @@ call portable.bat
 
 @set OnConnect=-
 
-::@set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %PARMS%\audiostream_EEG67+EOG3+SYNC2+VMRK_Quickamp_500Hz.prm 
-@set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %PARMS%\gUSBampsBBAAA-SchalkCap64+Audio2.prm
+@set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %PARMS%\gUSBampsBBAAA-Cap16+Audio2.prm
+::@set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %PARMS%\gUSBampsBBAAA-SchalkCap64+Audio2.prm
 @set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %PARMS%\drifting.prm 
+
+@if [%SESSION%] == [001] goto SKIPFIXED
+:FIXED
 @set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %PARMS%\fixed.prm 
+:SKIPFIXED
+
 @set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %PARMS%\audiostream_wadsworth_devel.prm
 @set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %WD%\subject_attention.prm 
 ::@set OnConnect=%OnConnect% ; LOAD PARAMETERFILE %WD%\subject_perception.prm 
@@ -35,7 +45,7 @@ start           gUSBampSource --RecordMic=1
 ::start           PythonSource             --PythonSrcWD=%WD%\python --PythonSrcShell=1 --PythonSrcLog=%WD%\log\###-src.txt
 
 ::start           DummySignalProcessing
-start           PythonSignalProcessing   --PythonSigWD=%WD%\python &&:: --PythonSigLog=%WD%\log\###-sig.txt
+start           PythonSignalProcessing   --PythonSigWD=%WD%\python
 
 ::start           DummyApplication
-start PythonApplication        --PythonAppWD=%WD%\python &&:: --PythonAppLog=%WD%\log\###-app.txt
+start PythonApplication        --PythonAppWD=%WD%\python
