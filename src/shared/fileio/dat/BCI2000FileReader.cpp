3bd33160d8da578df2fd5ffa7966460b0bb66947
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2011-11-08 16:40:45 +0000

    BCI2000FileReader now properly closes its file handle from the destructor.
    git-svn-id: http://www.bci2000.org/svn/trunk@3648 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/shared/fileio/dat/BCI2000FileReader.cpp
+++ src/shared/fileio/dat/BCI2000FileReader.cpp
@@ -5,23 +5,23 @@
 //              BCI2000 data file.
 //
 // $BEGIN_BCI2000_LICENSE$
-// 
+//
 // This file is part of BCI2000, a platform for real-time bio-signal research.
 // [ Copyright (C) 2000-2011: BCI2000 team and many external contributors ]
-// 
+//
 // BCI2000 is free software: you can redistribute it and/or modify it under the
 // terms of the GNU General Public License as published by the Free Software
 // Foundation, either version 3 of the License, or (at your option) any later
 // version.
-// 
+//
 // BCI2000 is distributed in the hope that it will be useful, but
 //                         WITHOUT ANY WARRANTY
 // - without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 // A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 // $END_BCI2000_LICENSE$
 ////////////////////////////////////////////////////////////////////////////////
 #include "PCHIncludes.h"
@@ -112,8 +112,7 @@ BCI2000FileReader::BCI2000FileReader( const char* inFileName )
 // **************************************************************************
 BCI2000FileReader::~BCI2000FileReader()
 {
-  delete mpStatevector;
-  delete[] mpBuffer;
+  Reset();
 }
 
 // **************************************************************************
@@ -453,7 +452,7 @@ BCI2000FileReader::BufferSample( long long inSample )
     mBufferEnd = mBufferBegin;
     while( !::feof( mpFile ) && !::ferror( mpFile ) && ( mBufferEnd - mBufferBegin < StateVectorLength() ) )
     {
-      int bytesRead = ::fread( mpBuffer + ( mBufferEnd - mBufferBegin ), 1, 
+      int bytesRead = ::fread( mpBuffer + ( mBufferEnd - mBufferBegin ), 1,
                                mBufferSize - static_cast<size_t>( mBufferEnd - mBufferBegin ), mpFile );
       mBufferEnd += bytesRead;
     }
