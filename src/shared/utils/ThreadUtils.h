d18b6845ff16481b0a1367eec35bd580d82d479c
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2011-12-09 15:41:29 +0000

    Added a ThreadUtils header file that presents OSThread static member functions as independent functions in its own namespace. This is to work around a compiler bug involving private inheritance from OSThread.
    git-svn-id: http://www.bci2000.org/svn/trunk@3687 7ef59cdf-0624-0410-a474-e34ce222aafa

---
+++ src/shared/utils/ThreadUtils.h
@@ -0,0 +1,53 @@
+//////////////////////////////////////////////////////////////////////
+// $Id$
+// Author: juergen.mellinger@uni-tuebingen.de
+// Description: A header file that presents OSThread's static member
+//   functions inside an additional namespace. This is to work around a
+//   compiler bug that disallows the use of public static members
+//   of a base class when there is private inheritance present in
+//   the inheritance chain.
+//
+// $BEGIN_BCI2000_LICENSE$
+//
+// This file is part of BCI2000, a platform for real-time bio-signal research.
+// [ Copyright (C) 2000-2011: BCI2000 team and many external contributors ]
+//
+// BCI2000 is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// BCI2000 is distributed in the hope that it will be useful, but
+//                         WITHOUT ANY WARRANTY
+// - without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// $END_BCI2000_LICENSE$
+///////////////////////////////////////////////////////////////////////
+#ifndef THREAD_UTILS_H
+#define THREAD_UTILS_H
+
+#include "OSThread.h"
+
+namespace ThreadUtils
+{
+
+bool IsMainThread()
+    { return OSThread::IsMainThread(); }
+
+void SleepFor( int ms ) // sleep for milliseconds
+	{ OSThread::SleepFor( ms ); }
+
+void PrecisionSleepFor( double ms ) // sleep for milliseconds
+    { OSThread::PrecisionSleepFor( ms ); }
+
+void PrecisionSleepUntil( PrecisionTime wakeup ) // sleep until absolute wakeup time
+    { OSThread::PrecisionSleepUntil( wakeup ); }
+
+} // namespace ThreadUtils
+
+#endif // THREAD_UTILS_H
+
