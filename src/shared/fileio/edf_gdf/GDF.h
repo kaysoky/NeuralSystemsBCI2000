bd77edb557c15224eb3f34643813f63e573f4efd
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2012-01-30 15:38:12 +0000

    gcc compilation.
    git-svn-id: http://www.bci2000.org/svn/trunk@3786 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/shared/fileio/edf_gdf/GDF.h
+++ src/shared/fileio/edf_gdf/GDF.h
@@ -6,23 +6,23 @@
 //              GDF 2.10 data format as defined in Schloegl et al, 2008.
 //
 // $BEGIN_BCI2000_LICENSE$
-// 
+//
 // This file is part of BCI2000, a platform for real-time bio-signal research.
 // [ Copyright (C) 2000-2011: BCI2000 team and many external contributors ]
-// 
+//
 // BCI2000 is free software: you can redistribute it and/or modify it under the
 // terms of the GNU General Public License as published by the Free Software
 // Foundation, either version 3 of the License, or (at your option) any later
 // version.
-// 
+//
 // BCI2000 is distributed in the hope that it will be useful, but
 //                         WITHOUT ANY WARRANTY
 // - without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 // A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 // $END_BCI2000_LICENSE$
 ////////////////////////////////////////////////////////////////////////////////
 #ifndef GDF_H
@@ -231,7 +231,7 @@ template<class U>
 GDF::Num<T, N>::Num( const U& u )
 {
   for( int i = 0; i < N; ++i )
-    mValues[ i ] = static_cast<T::ValueType>( u );
+    mValues[ i ] = static_cast<typename T::ValueType>( u );
 }
 
 template<class T, int N>
