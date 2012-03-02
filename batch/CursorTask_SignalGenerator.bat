50ef7d2ac744fc7137fc397c236afe2043d928a3
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2012-03-02 14:24:59 +0000

    Reverted accidentally committed change to CursorTask_SignalGenerator batch file.
    git-svn-id: http://www.bci2000.org/svn/trunk@3850 7ef59cdf-0624-0410-a474-e34ce222aafa

--- batch/CursorTask_SignalGenerator.bat
+++ batch/CursorTask_SignalGenerator.bat
@@ -27,6 +27,6 @@ start operator.exe ^
    --Title %~n0 ^
    --OnConnect "-LOAD PARAMETERFILE ..\parms\examples\CursorTask_SignalGenerator.prm"
 start SignalGenerator.exe --EvaluateTiming=0 127.0.0.1
-rem start SpectralSignalProcessing.exe 127.0.0.1
+start SpectralSignalProcessing.exe 127.0.0.1
 start CursorTask.exe 127.0.0.1
 cd ..
