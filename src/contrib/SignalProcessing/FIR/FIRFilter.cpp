59ab1f8f7add339ea4d22ff71e3fdd2f07c75c95
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2012-03-06 15:44:53 +0000

    Fixed std::inner_product end iterator error in FIRFilter.
    git-svn-id: http://www.bci2000.org/svn/trunk@3865 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/contrib/SignalProcessing/FIR/FIRFilter.cpp
+++ src/contrib/SignalProcessing/FIR/FIRFilter.cpp
@@ -3,23 +3,23 @@
 // Description: A finite impulse response (FIR) filter for temporal filtering.
 //
 // $BEGIN_BCI2000_LICENSE$
-// 
+//
 // This file is part of BCI2000, a platform for real-time bio-signal research.
 // [ Copyright (C) 2000-2012: BCI2000 team and many external contributors ]
-// 
+//
 // BCI2000 is free software: you can redistribute it and/or modify it under the
 // terms of the GNU General Public License as published by the Free Software
 // Foundation, either version 3 of the License, or (at your option) any later
 // version.
-// 
+//
 // BCI2000 is distributed in the hope that it will be useful, but
 //                         WITHOUT ANY WARRANTY
 // - without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 // A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 // $END_BCI2000_LICENSE$
 ////////////////////////////////////////////////////////////////////////////////
 #include "PCHIncludes.h"
@@ -125,7 +125,7 @@ FIRFilter::Process( const GenericSignal& Input, GenericSignal& Output )
     // Compute buffer's convolution with coefficient vector.
     DataVector result( 0.0, inputLength );
     for( int sample = 0; sample < inputLength; ++sample )
-      result[sample] = inner_product( &mFilter[channel][0], &mFilter[channel][filterLength-1], &mBuffer[channel][sample], 0.0 );
+      result[sample] = inner_product( &mFilter[channel][0], &mFilter[channel][filterLength], &mBuffer[channel][sample], 0.0 );
     // Compute output.
     switch( mFIRIntegration )
     {
