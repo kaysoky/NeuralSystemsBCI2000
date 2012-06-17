8faef0d8ea370819458103cb53419af296d6db9c
jhill <jhill@7ef59cdf-0624-0410-a474-e34ce222aafa>
2012-06-17 04:50:27 +0000

    BCPy2000 __version__ number corrected
    git-svn-id: http://www.bci2000.org/svn/trunk@4145 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/contrib/BCPy2000/framework/BCPy2000/__init__.py
+++ src/contrib/BCPy2000/framework/BCPy2000/__init__.py
@@ -24,7 +24,7 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-__version__   = '41390'
+__version__   = '41450'
 __author__    = 'Jeremy Hill, Thomas Schreiner, Christian Puzicha, Jason Farquhar'
 __copyright__ = 'Copyright (c) 2007-11'
 __email__     = 'bcpy2000@bci2000.org'
