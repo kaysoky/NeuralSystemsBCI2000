cc3d83468232ac12bfe29df55720eb92b47ad3b4
jhill <jhill@7ef59cdf-0624-0410-a474-e34ce222aafa>
2012-06-22 23:24:49 +0000

    first working full-integrated version of the SessionLauncher
    git-svn-id: http://www.bci2000.org/svn/trunk@4187 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/contrib/BCPy2000/framework/BCPy2000/private/SessionLauncher.py
+++ src/contrib/BCPy2000/framework/BCPy2000/private/SessionLauncher.py
@@ -7,10 +7,13 @@ class SessionGUI( tk.Tk ):
 	def __init__( self, settingsfile=None, go=True ):
 		tk.Tk.__init__( self )
 		self.minsize( width=800, height=200 )
+		self.option_add("*Font", "40")
 		
 		if settingsfile == None: settingsfile = 'SessionGUISettings.txt'
 		self.__settingsfile = settingsfile
 		self.__optionprefix = '_PossibleValuesFor'
+		self.__bg = '#DDDDDD'
+		self.__valid = False
 		
 		self.__settings = {
 			'Subject':    'TestSubject',
@@ -37,12 +40,12 @@ class SessionGUI( tk.Tk ):
 		else: self.update()
 				
 	def LoadSettings( self ):
-		if not os.path.isfile( self.__settingsfile ): self.SaveSettings()
-		self.__settings.update( eval( '\n'.join( open( self.__settingsfile, 'rt' ).readlines() ) ) )
+		if not os.path.isfile( self.__settingsfile ): self.SaveSettings( self.ReportSettings() )
+		self.__saved_settings_content = '\n'.join( open( self.__settingsfile, 'rt' ).readlines() )
+		self.__settings.update( eval( self.__saved_settings_content ) )
 		
-	def ReportSettings( self, file=None ):
-		if file == None: file = sys.stdout
-		if isinstance( file, basestring ): file = open( file, 'wt' )
+	def ReportSettings( self ):
+		t = ''
 		s = dict( self.__settings )
 		args = s[ '_Arguments' ].split()
 		args.append( None )
@@ -50,70 +53,84 @@ class SessionGUI( tk.Tk ):
 		args.append( None )
 		keys = args + sorted( set( s.keys() ) - set( args ) )
 		maxlen = max( [ len(k) for k in keys if k != None ] )
-		file.write( '{\n' )
+		t += '{\n'
 		for k in keys:
-			if k == None: file.write( '\n' )
-			else: file.write( ' ' * ( maxlen - len( k ) + 4 ) + repr(k) + ':   ' + repr( s[ k ] ) + ',\n' )
-		file.write( '}\n' )
+			if k == None: t += '\n'
+			else: t += ' ' * ( maxlen - len( k ) + 4 ) + repr(k) + ':   ' + repr( s[ k ] ) + ',\n'
+		t += '}\n'
+		return t
 			
-	def SaveSettings( self ):
-		self.ReportSettings( file=self.__settingsfile )
+	def SaveSettings( self, txt ):
+		open( self.__settingsfile, 'wt' ).write( txt )
+		self.__saved_settings_content = txt
 	
 	def Render( self ):
-	
-		self.title( self.__settings[ '_Title' ] )
-		
+		title = self.__settings[ '_Title' ]
+		try: import win32gui
+		except ImportError: pass
+		else:
+			h = win32gui.FindWindow( 0, title )
+			if h != 0:
+				tk.Label( self, text='Another window called "%s" is already open.\nClose this one, and use the original.' % title ).pack()
+				return
+			
+		self.title( title )
+		self.configure( background=self.__bg )
+
 		mf = self.__masterframe
 		if mf != None: mf.destroy()
-		self.__masterframe = mf = tk.Frame( self ); mf.pack( fill='both' )
+		self.__masterframe = mf = tk.Frame( self, background=self.__bg ); mf.pack( fill='both' )
 		
-		left = tk.Frame( mf ); left.pack( side='left' )
+		left = tk.Frame( mf, background=self.__bg ); left.pack( side='left' )
 		fr = left
-		#fr = tk.Frame( mf ); #fr.pack( fill='x' )
+		#fr = tk.Frame( mf, background=self.__bg ); #fr.pack( fill='x' )
 		
 		row  = 0
 		
 		for row, varName in enumerate( self.__settings[ '_Arguments' ].split() ):
 			
-			w = self.__widgets[ varName + 'Label' ] = tk.Label( fr, text=varName + ': ' )
+			w = self.__widgets[ varName + 'Label' ] = tk.Label( fr, text=varName + ': ', background=self.__bg )
 			w.grid( row=row, column=0, sticky='e' )
 			optionsKey = self.__optionprefix + varName
 			if optionsKey in self.__settings:
 				v = self.__inputs[ varName ] = tk.StringVar( value=self.__settings[ varName ] )
-				w = self.__widgets[ varName + 'Input' ] = tk.OptionMenu( fr, v, *self.__settings[ optionsKey ], command=self.UpdateVars )
+				w = self.__widgets[ varName + 'Input' ] = tk.OptionMenu( fr, v, *self.__settings[ optionsKey ] )
+				#w.configure( command=self.UpdateSettings )
 			else:
-				w = self.__widgets[ varName + 'Input' ] = self.__inputs[ varName ] = tk.Entry( fr, validatecommand=self.UpdateVars )
+				w = self.__widgets[ varName + 'Input' ] = self.__inputs[ varName ] = tk.Entry( fr, validatecommand=self.UpdateSettings )
 				w.delete( 0, tk.END )
 				w.insert( 0, self.__settings[ varName ] )
 			w.grid( row=row, column=1, sticky='w' )
 		
-		right = tk.Frame( mf, background='#FFFFFF' ); right.pack( side='left', padx=50 )
+		right = tk.Frame( mf, background=self.__bg ); right.pack( side='left', padx=50 )
 		fr = right
 
 		self.dirLabels = {}
-		self.dirLabels[ 'CALIB' ] = w = tk.Label( fr, text='(calibration directory info)'); w.pack( fill='x' )
-		self.dirLabels[ 'FREE' ]  = w = tk.Label( fr, text='(free-choice directory info)'); w.pack( fill='x' )
+		self.dirLabels[ 'CALIB' ] = w = tk.Label( fr, text='(calibration directory info)'); w.pack( fill='x', anchor='w' )
+		self.dirLabels[ 'FREE' ]  = w = tk.Label( fr, text='(free-choice directory info)'); w.pack( fill='x', anchor='w' )
 
-		#footer = tk.Frame( mf ); footer.pack( fill='x' )
+		#footer = tk.Frame( mf, background=self.__bg ); footer.pack( fill='x' )
 		#fr = footer
 
 		self.transferWeightsButton = w = tk.Button( fr, text='Transfer weights CALIB->FREE', command=self.TransferChosenWeights ); w.pack( side='left' )
-		self.launchBCI2000Button = w = tk.Button( self, text='Launch BCI2000', command=self.LaunchBCI2000 ); w.pack( side='left' )
-		self.quitBCI2000Button = w = tk.Button( self, text='Quit BCI2000', command=self.QuitBCI2000 ); w.pack( side='left' )
-		self.reloadWeightsButton = w = tk.Button( self, text='Reload weights', command=self.ReloadChosenWeights ); w.pack( side='left' )
-		self.exitButton = w = tk.Button( self, text='Exit', command=self.destroy ); w.pack( side='left' )
+		self.launchBCI2000Button = w = tk.Button( self, text='Launch BCI2000', command=self.LaunchBCI2000 ); w.pack( side='left', padx=20 )
+		self.quitBCI2000Button = w = tk.Button( self, text='Quit BCI2000', command=self.QuitBCI2000 ); w.pack( side='left', padx=20 )
+		self.reloadWeightsButton = w = tk.Button( self, text='Reload weights', command=self.ReloadChosenWeights ); w.pack( side='left', padx=20 )
+		self.exitButton = w = tk.Button( self, text='Exit', command=self.destroy ); w.pack( side='left', padx=20 )
 
-		self.UpdateVars()
+		self.__valid = True
+		self.UpdateSettings()
 		for w in self.__widgets.values():
 			if isinstance( w, tk.Entry ): w.configure( validate='focusout' ) # 'all' would be nice but it always seems to lag one keystroke behind :-<
-			
-	def UpdateVars( self, *pargs ):
-		for k in self.__inputs: self.__settings[ k ] = self.__inputs[ k ].get()
-		self.SaveSettings()
-		self.UpdateDisplay()
-		return True
 		
-	def UpdateDisplay( self ):
+		
+		
+	def UpdateSettings( self ):
+		if not self.__valid: return False
+		for k in self.__inputs: self.__settings[ k ] = self.__inputs[ k ].get()
+		sc = self.ReportSettings()
+		if sc != self.__saved_settings_content: self.SaveSettings( sc )
+			
 		for mode in [ 'CALIB', 'FREE' ]:
 			d = self.GetDataDirectory( mode=mode )
 			s = 'directory ' + self.GetSubjectDirName( mode=mode )
@@ -130,7 +147,7 @@ class SessionGUI( tk.Tk ):
 				s += ' does not yet exist'
 			s += '\n'
 			if self.__settings[ 'Mode' ] == mode: bg = '#FFFF00'
-			else: bg = '#FFFFFF'
+			else: bg = self.__bg
 			self.dirLabels[ mode ].configure( text=s, justify='left', background=bg )
 
 		if os.path.isfile( self.GetChosenWeightsPath() ): self.reloadWeightsButton.configure( state='normal' )
@@ -141,7 +158,9 @@ class SessionGUI( tk.Tk ):
 		
 		try: self.after_cancel( self.__afterid )
 		except: pass
-		self.__afterid = self.after( 500, self.UpdateDisplay )
+		self.__afterid = self.after( 500, self.UpdateSettings )
+		#import time; print time.time()
+		return True
 
 	def GetShellExecutable( self ):
 		return os.path.realpath( os.path.join( self.__settings[ '_ProgDir' ], 'BCI2000Shell' ) )
@@ -174,32 +193,32 @@ class SessionGUI( tk.Tk ):
 		return os.system( self.GetShellExecutable() + ' -c ' + cmd )
 			
 	def LaunchBCI2000( self ):
-		self.UpdateVars() # because of the lag
+		self.UpdateSettings() # because of the lag
 		os.system( self.GetLaunchCommand() )
-		self.UpdateDisplay()
+		self.UpdateSettings()
 		
 	def QuitBCI2000( self ):
-		self.UpdateVars() # because of the lag
+		self.UpdateSettings() # because of the lag
 		self.ExecuteInShell( 'quit' )
-		self.UpdateDisplay()
+		self.UpdateSettings()
 		
 	def ReloadChosenWeights( self ):
-		self.UpdateVars() # because of the lag
+		self.UpdateSettings() # because of the lag
 		self.ExecuteInShell( 'load parameterfile "' + self.GetChosenWeightsPath() + '"' )
 		self.ExecuteInShell( 'setconfig' )
-		self.UpdateDisplay()
+		self.UpdateSettings()
 
 	def TransferChosenWeights( self ):
-		self.UpdateVars() # because of the lag
+		self.UpdateSettings() # because of the lag
 		srcfile = self.GetChosenWeightsPath( mode='CALIB' )
 		freedir = self.GetDataDirectory( mode='FREE' )
 		if not os.path.isdir( freedir ): os.mkdir( freedir )
 		dstfile = self.GetChosenWeightsPath( mode='FREE' )
 		shutil.copyfile( srcfile, dstfile )
-		self.UpdateDisplay()
+		self.UpdateSettings()
 
 	def destroy( self ):
-		self.UpdateVars() # because of the lag
+		self.UpdateSettings() # because of the lag
 		# other cleanup
 		tk.Tk.destroy( self )
 
