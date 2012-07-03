447698c6d0428e7c469b1859547238bd6fda28bb
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2012-07-03 15:13:41 +0000

    MinGW compilation.
    git-svn-id: http://www.bci2000.org/svn/trunk@4228 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/shared/utils/RandomGenerator.h
+++ src/shared/utils/RandomGenerator.h
@@ -7,23 +7,23 @@
 //   random numbers.
 //
 // $BEGIN_BCI2000_LICENSE$
-// 
+//
 // This file is part of BCI2000, a platform for real-time bio-signal research.
 // [ Copyright (C) 2000-2012: BCI2000 team and many external contributors ]
-// 
+//
 // BCI2000 is free software: you can redistribute it and/or modify it under the
 // terms of the GNU General Public License as published by the Free Software
 // Foundation, either version 3 of the License, or (at your option) any later
 // version.
-// 
+//
 // BCI2000 is distributed in the hope that it will be useful, but
 //                         WITHOUT ANY WARRANTY
 // - without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 // A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 // $END_BCI2000_LICENSE$
 ////////////////////////////////////////////////////////////////////////////////
 #ifndef RANDOM_GENERATOR_H
@@ -39,7 +39,7 @@ class RandomGenerator : private EnvironmentExtension
   typedef uint32_t NumberType;
 
 #ifdef TODO
-# error Modify ID determination to use a filter's unique ID once multiple filter instances are possible.
+# error Modify ID determination to use a filter´s unique ID once multiple filter instances are possible.
 #endif
   template<typename T>
   explicit RandomGenerator( const T* t )
