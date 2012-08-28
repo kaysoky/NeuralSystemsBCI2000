3839998ceba1bc98f208c45db7a6f0cb7d922147
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2012-08-28 18:35:09 +0000

    Replaced "FilterFilter" by "IIRBandpass" in PipeDefinition template.
    git-svn-id: http://www.bci2000.org/svn/trunk@4271 7ef59cdf-0624-0410-a474-e34ce222aafa

--- build/buildutils/BootstrapCustomProjects/templates/PipeDefinition.cpp
+++ build/buildutils/BootstrapCustomProjects/templates/PipeDefinition.cpp
@@ -2,26 +2,26 @@
 // $Id: $
 // Authors:
 // Description: Pipe definition for the ` module.
-//              
+//
 //
 // $BEGIN_BCI2000_LICENSE$
-// 
+//
 // This file is part of BCI2000, a platform for real-time bio-signal research.
 // [ Copyright (C) 2000-2012: BCI2000 team and many external contributors ]
-// 
+//
 // BCI2000 is free software: you can redistribute it and/or modify it under the
 // terms of the GNU General Public License as published by the Free Software
 // Foundation, either version 3 of the License, or (at your option) any later
 // version.
-// 
+//
 // BCI2000 is distributed in the hope that it will be useful, but
 //                         WITHOUT ANY WARRANTY
 // - without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 // A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 // $END_BCI2000_LICENSE$
 ////////////////////////////////////////////////////////////////////////////////
 #include "PCHIncludes.h"
@@ -33,9 +33,9 @@
 // the BCI2000 framework will then determine the order of the filters by string-sorting
 // these location tokens.  By convention:
 //   - filters locations for SignalSource modules begin with "1."
-//   - filters locations for SignalProcessing modules begin with "2."  
+//   - filters locations for SignalProcessing modules begin with "2."
 //   - filters locations Application modules begin with "3."
-// 
+//
 // Locations defined here using the Filter() macro override the defaults set by
 // RegisterFilter() in the individual filters' implementation files.
 
@@ -46,8 +46,8 @@
 //#include "SpatialFilter.h"
 //Filter( SpatialFilter, 2.B );
 
-//#include "FilterFilter.h"
-//Filter( FilterFilter, 2.B3 );
+//#include "IIRBandpass.h"
+//Filter( IIRBandpass, 2.B3 );
 
 //#include "ARFilter.h"
 //Filter( ARFilter, 2.C );
