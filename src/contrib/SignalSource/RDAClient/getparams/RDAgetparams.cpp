bc066213c78f3e2d58ecc752334070c9824a06de
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2012-10-24 18:07:35 +0000

    Disabled SpatialFilter in RDAgetparams output configuration.
    git-svn-id: http://www.bci2000.org/svn/trunk@4292 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/contrib/SignalSource/RDAClient/getparams/RDAgetparams.cpp
+++ src/contrib/SignalSource/RDAClient/getparams/RDAgetparams.cpp
@@ -5,23 +5,23 @@
 //   from the BrainVisionRecorder, and write them out in BCI2000 parameter format.
 //
 // $BEGIN_BCI2000_LICENSE$
-// 
+//
 // This file is part of BCI2000, a platform for real-time bio-signal research.
 // [ Copyright (C) 2000-2012: BCI2000 team and many external contributors ]
-// 
+//
 // BCI2000 is free software: you can redistribute it and/or modify it under the
 // terms of the GNU General Public License as published by the Free Software
 // Foundation, either version 3 of the License, or (at your option) any later
 // version.
-// 
+//
 // BCI2000 is distributed in the hope that it will be useful, but
 //                         WITHOUT ANY WARRANTY
 // - without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 // A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 // $END_BCI2000_LICENSE$
 ///////////////////////////////////////////////////////////////////////////////////
 #include <iostream>
@@ -94,8 +94,9 @@ int main( int argc, char** argv )
     "RDA int SampleBlockSize= 1 ",
     "RDA intlist TransmitChList= 0 ",
     "RDA matrix SpatialFilter= 0 1 ",
-    "RDA int SourceMax= 300muV ",
-    "RDA int SourceMin= -300muV ",
+    "RDA int SpatialFilterType= 0 ",
+    "RDA float SourceMax= 300muV ",
+    "RDA float SourceMin= -300muV ",
   };
   for( size_t i = 0; i < sizeof( params ) / sizeof( *params ); ++i )
     paramlist.Add( ( string( params[ i ] ) + " // getparams " + hostname ).c_str() );
