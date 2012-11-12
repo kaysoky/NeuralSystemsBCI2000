kaysoky/NeuralSystemsBCI2000

commit fbfe448acc3107f8bffe0fe782d97ca27d112c56

Fixed P300Classifier to work with single-column or single-row speller configurations.
git-svn-id: http://www.bci2000.org/svn/trunk@4291 7ef59cdf-0624-0410-a474-e34ce222aafa

src/core/Tools/P300Classifier/P300_Classify.cpp
@@ -1,4 +1,6 @@
 #include "P300_Classify.h"
+#include <cassert>
+#include <set>
 
 ///////////////////////////////////////////////////////////////////
 /// Predict the intended letters according to the pscore matrix.
@@ -28,13 +30,14 @@ void P300_Classify(ap::real_2d_array pscore,
 				   vector<double>& result,
 				   vector<string>& predicted)
 {
-
+// jm, Oct 24, 2012
+if( NumMatrixRows < 2 ) NumMatrixRows = 0;
+if( NumMatrixColumns < 2 ) NumMatrixColumns = 0;
 ////////////////////////////////////////////////////////////////////////
 // Section: Define variables
 int choice, epoch;
 double val, max_value_row= -1e15, max_value_col= -1e15, numletters, correct;
 ap::real_2d_array cflash;
-ap::real_2d_array score;
 ap::template_2d_array<int, true> predictedcol;
 ap::template_2d_array<int, true> predictedrow;
 vector<int> range;
@@ -47,7 +50,6 @@ numletters = (pscore.gethighbound(0)+1)/NumberOfSequences;
 choice = NumMatrixRows + NumMatrixColumns;
 epoch = NumberOfSequences * choice;
 cflash.setbounds(0, choice-1, 0, pscore.gethighbound(0));
-score.setbounds(0, static_cast<int>(numletters-1), 0, choice-1);
 predictedrow.setbounds(0, static_cast<int>(numletters-1), 0, NumberOfSequences-1);
 predictedcol.setbounds(0, static_cast<int>(numletters-1), 0, NumberOfSequences-1);
 
@@ -62,6 +64,7 @@ for (int i=0; i<numletters; i++)
 	}
 	if (range.size() != 0)
 	{
+#if 0
 		for (size_t j=0; j<range.size(); j++)
 			codes.push_back(StimulusCode(range[j])*StimulusType(range[j]));
 
@@ -75,6 +78,26 @@ for (int i=0; i<numletters; i++)
 			coderow.push_back(codes[2]);
 		
 		codes.clear();
+#else // jm Oct 24, 2012 NOTE: This functions seems to invert the meaning of Rows and Columns
+    {
+      set<int> codes;
+	    if( NumMatrixRows < 2 )
+	      codes.insert( NumMatrixColumns + 1 );
+	    if( NumMatrixColumns < 2 )
+	      codes.insert( 1 );
+		  for (size_t j=0; j<range.size(); j++)
+			  codes.insert(StimulusCode(range[j])*StimulusType(range[j]));
+
+      assert( codes.size() == 3 );
+	    set<int>::const_iterator i = codes.begin();
+	    if( i != codes.end() )
+	      ++i;
+	    if( i != codes.end() )
+	      codecol.push_back( *i++ );
+	    if( i != codes.end() )
+	      coderow.push_back( *i++ );
+	  }
+#endif
 		range.clear();
 
 		for (int j=0; j<choice; j++)
@@ -85,24 +108,27 @@ for (int i=0; i<numletters; i++)
 				val += pscore(j,k);
 				cflash(j,k) = val;
 			}
-			score(i,j) = val;
 		}		
 	}
 }
 for (int i=0; i<numletters; i++)
 {
 	int l = 0;
-	for (int k=i*NumberOfSequences; k<(i+1)*NumberOfSequences; k++)
+  max_value_col= -1e15, max_value_row= -1e15;
+  for (int k=i*NumberOfSequences; k<(i+1)*NumberOfSequences; k++)
 	{
 		for (int j=0; j<choice; j++)
 		{
+		  int code = j + 1;
+		  if( NumMatrixColumns < 1 )
+		    code += 1;
 			if ((j<NumMatrixColumns))
 			{
 				/* compare with max */
 				if (cflash(j,k) > max_value_col)
 				{
 					max_value_col = cflash(j,k);
-					predictedcol(i,l) = j + 1;
+					predictedcol(i,l) = code;
 				}
 			}
 			if ((j>=NumMatrixColumns))
@@ -111,13 +137,23 @@ for (int i=0; i<numletters; i++)
 				if (cflash(j,k) > max_value_row)
 				{
 					max_value_row = cflash(j,k);
-					predictedrow(i,l) = j + 1;
+					predictedrow(i,l) = code;
 				}
 			}
 		}
-		l++, max_value_col= -1e15, max_value_row= -1e15;
+		// jm Oct 24, 2012
+		if( NumMatrixColumns < 1 )
+		  predictedcol( i, l ) = 1;
+		if( NumMatrixRows < 1 )
+		  predictedrow( i, l ) = NumMatrixColumns + 1;
+		  
+		l++;
 	}
 }
+
+// jm
+if( NumMatrixColumns < 1 )
+  NumMatrixColumns = 1;
 
 for (int j=0; j<NumberOfSequences; j++)
 {
@@ -125,12 +161,8 @@ for (int j=0; j<NumberOfSequences; j++)
 	for (int i=0; i<numletters; i++)
 	{
 		if ((coderow[i] == predictedrow(i,j)) && codecol[i] == predictedcol(i,j))
-		{
-			predicted.push_back(TargetDefinitions[(NumMatrixColumns*(predictedrow(i,j)-1-NumMatrixColumns))+predictedcol(i,j)-1]);
 			correct++;
-		}
-		else
-			predicted.push_back(TargetDefinitions[(NumMatrixColumns*(predictedrow(i,j)-1-NumMatrixColumns))+predictedcol(i,j)-1]);
+	  predicted.push_back(TargetDefinitions[NumMatrixColumns*(predictedrow(i,j)-1-NumMatrixColumns)+predictedcol(i,j)-1]);
 	}
 	result.push_back((correct/numletters)*100);
 }

src/core/Tools/P300Classifier/pages.cpp
@@ -2,12 +2,37 @@
 #include <QtCore/QTextStream>
 #include "configdialog.h"
 #include "pages.h"
+#include <set>
 
 GenerateFeatureWeightsThread *g_pGenerateFeatureWeightsThread = NULL;
 DataPage *g_pDataPage = NULL;
 ParametersPage *g_pParametersPage = NULL;
 DetailsPage *g_pDetailsPage = NULL;
 QErrorMessage *errorMessageDialog = NULL;
+
+// jm, Oct 24, 2012
+static int FixupStimulusCodes( ap::template_1d_array<unsigned short int, true>& ioCodes )
+{
+  set<int> codes;
+  for( int i = 0; i <= ioCodes.gethighbound(0); ++i ) 
+    codes.insert( ioCodes( i ) );
+  codes.erase( 0 );
+  if( !codes.empty() )
+  {
+    int minCode = *codes.begin();
+    if( minCode > 1 )
+      for( int i = 0; i <= ioCodes.gethighbound(0); ++i ) 
+        ioCodes( i ) -= ( minCode - 1 );
+  }
+  return static_cast<int>( codes.size() );
+}
+
+static void Reset()
+{
+  g_pDetailsPage->Clear();
+  g_pDataPage->findChild<QProgressBar*>()->setValue( 0 );
+}
+// end jm
 
 void callback_status(string message)
 {
@@ -288,6 +313,19 @@ DetailsPage::DetailsPage(QWidget *parent)
   g_pDetailsPage = this;
 }
 
+void
+DetailsPage::Clear()
+{
+  QList<QLineEdit*> fields = findChildren<QLineEdit*>();
+  for( int i = 0; i < fields.size(); ++i )
+    fields[i]->setText( "" );
+  QList<QTextEdit*> fields2 = findChildren<QTextEdit*>();
+  for( int i = 0; i < fields2.size(); ++i )
+    fields2[i]->setText( "" );
+  findChild<QProgressBar*>()->setValue( 0 );
+}
+
+
 void ParametersPage::setDataPage(DataPage *dataPage)
 {
   pDataPage = dataPage;
@@ -362,6 +400,8 @@ void ParametersPage::ChSetTextHasChanged()
 
 void DataPage::LoadSettings()
 {
+  Reset();
+  
   QString inicfgFile = this->inicfgFileLineEdit->text();
   string szFile = this->inicfgFileLineEdit->text().toLocal8Bit().constData();
   char format = 'f';
@@ -424,6 +464,7 @@ void DataPage::LoadTrainingButton()
 
   if (files.count()>0)
   {
+    Reset();
     this->TrainingDataFilesList->clear();
     fPathArr_TrainingData.clear();
 
@@ -1844,13 +1885,11 @@ void GenerateFeatureWeightsThread::run()
   }
   emit signalProgressBar(1, 5, 3);
 
-  if (g_pDataPage->mode_TrainingData == 1 || g_pDataPage->mode_TrainingData == 2)
-    // Get score for input signal
-    NumberOfChoices = parms.NumMatrixRows + parms.NumMatrixColumns;
-  if (g_pDataPage->mode_TrainingData == 3 || g_pDataPage->mode_TrainingData == 4)
-    NumberOfChoices = static_cast<int>(state.TargetDefinitions.size());
-
-  if (!GetScore(signal_all_files, state.StimulusCode, g_pDataPage->tMUD, state.trialnr, windowlen, row_chset,
+  // jm Oct 24, 2012
+  ap::template_1d_array<unsigned short int, true> stimCode_GetScore = state.StimulusCode;
+  NumberOfChoices = FixupStimulusCodes( stimCode_GetScore );
+  
+  if (!GetScore(signal_all_files, stimCode_GetScore, g_pDataPage->tMUD, state.trialnr, windowlen, row_chset,
       parms.NumberOfSequences, NumberOfChoices, g_pDataPage->mode_TrainingData, pscore))
   {
   // Cristhian modification April 17, 2010
@@ -2129,13 +2168,11 @@ void ApplyFeatureWeightsThread::run()
   emit signalProgressText(oss.str().c_str());
   oss.str("");
 
-  if (g_pDataPage->mode_TrainingData == 1 || g_pDataPage->mode_TrainingData == 2)
-    // Get score for input signal
-    NumberOfChoices = parms.NumMatrixRows + parms.NumMatrixColumns;
-  if (g_pDataPage->mode_TrainingData == 3 || g_pDataPage->mode_TrainingData == 4)
-    NumberOfChoices = static_cast<int>(state.TargetDefinitions.size());
+  // jm Oct 24, 2012
+  ap::template_1d_array<unsigned short int, true> stimCode_GetScore = state.StimulusCode;
+  NumberOfChoices = FixupStimulusCodes( stimCode_GetScore );
 
-  GetScore(signal_all_files, state.StimulusCode, g_pDataPage->tMUD, state.trialnr, windowlen, row_chset,
+  GetScore(signal_all_files, stimCode_GetScore, g_pDataPage->tMUD, state.trialnr, windowlen, row_chset,
       parms.NumberOfSequences, NumberOfChoices, g_pDataPage->mode_TrainingData, pscore);
 
   emit signalProgressBar(2, 5, 4);

src/core/Tools/P300Classifier/pages.h
@@ -206,6 +206,7 @@ class DetailsPage : public QWidget
 
 public:
     DetailsPage(QWidget *parent = 0);
+    void Clear();
 
 	QGroupBox		*ProgressGroup;
 	QGroupBox		*FileInfoGroup;

commit 9d26e15cfb3b55e18f536f07c8bd32d340ca377a

Fixed a bug introduced in r4291.
git-svn-id: http://www.bci2000.org/svn/trunk@4302 7ef59cdf-0624-0410-a474-e34ce222aafa

src/core/Tools/P300Classifier/P300_Classify.cpp
@@ -1,6 +1,7 @@
 #include "P300_Classify.h"
 #include <cassert>
 #include <set>
+#include <limits>
 
 ///////////////////////////////////////////////////////////////////
 /// Predict the intended letters according to the pscore matrix.
@@ -36,7 +37,8 @@ if( NumMatrixColumns < 2 ) NumMatrixColumns = 0;
 ////////////////////////////////////////////////////////////////////////
 // Section: Define variables
 int choice, epoch;
-double val, max_value_row= -1e15, max_value_col= -1e15, numletters, correct;
+static const double inf = std::numeric_limits<double>::infinity();
+double val, max_value_row= -inf, max_value_col= -inf, numletters, correct;
 ap::real_2d_array cflash;
 ap::template_2d_array<int, true> predictedcol;
 ap::template_2d_array<int, true> predictedrow;
@@ -113,10 +115,10 @@ for (int i=0; i<numletters; i++)
 }
 for (int i=0; i<numletters; i++)
 {
-	int l = 0;
-  max_value_col= -1e15, max_value_row= -1e15;
+	int s = 0;
   for (int k=i*NumberOfSequences; k<(i+1)*NumberOfSequences; k++)
 	{
+    max_value_col= -inf, max_value_row= -inf;
 		for (int j=0; j<choice; j++)
 		{
 		  int code = j + 1;
@@ -128,7 +130,7 @@ for (int i=0; i<numletters; i++)
 				if (cflash(j,k) > max_value_col)
 				{
 					max_value_col = cflash(j,k);
-					predictedcol(i,l) = code;
+					predictedcol(i,s) = code;
 				}
 			}
 			if ((j>=NumMatrixColumns))
@@ -137,17 +139,17 @@ for (int i=0; i<numletters; i++)
 				if (cflash(j,k) > max_value_row)
 				{
 					max_value_row = cflash(j,k);
-					predictedrow(i,l) = code;
+					predictedrow(i,s) = code;
 				}
 			}
 		}
 		// jm Oct 24, 2012
 		if( NumMatrixColumns < 1 )
-		  predictedcol( i, l ) = 1;
+		  predictedcol( i, s ) = 1;
 		if( NumMatrixRows < 1 )
-		  predictedrow( i, l ) = NumMatrixColumns + 1;
+		  predictedrow( i, s ) = NumMatrixColumns + 1;
 		  
-		l++;
+		s++;
 	}
 }
 