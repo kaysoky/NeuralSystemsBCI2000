b38a09289b09dfbbded82f046c0ee4989472cb2f
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2013-06-11 13:19:12 +0000

    Downloading Qt through CMake.
    git-svn-id: http://www.bci2000.org/svn/trunk@4486 7ef59cdf-0624-0410-a474-e34ce222aafa

--- build/cmake/FindQt.cmake
+++ build/cmake/FindQt.cmake
@@ -1,18 +1,85 @@
 ###########################################################################
 ## $Id$
-## Authors: griffin.milsap@gmail.com
+## Authors: juergen.mellinger@uni-tuebingen.de
 ## Description: Find Qt4
 
-IF( WIN32 AND CMAKE_SIZEOF_VOID_P EQUAL 4 AND ( MINGW OR MSVC90 OR MSVC10 ) )
-  OPTION( USE_EXTERNAL_QT
-    "Set to ON to dynamically link BCI2000 against an existing Qt installation.
-    Note that this will introduce various run-time dependencies which may be difficult
-    to manage when deploying the resulting executables."
-    OFF
-  )
-ELSE()
-  SET( USE_EXTERNAL_QT ON )
-ENDIF()
+
+FUNCTION( DOWNLOAD_EXTRACT_QT qtver arch qmake )
+
+  SET( qturl "http://www.bci2000.org/externals/qt/qt-${qtver}.${arch}" )
+  SET( basedir "${BCI2000_BINARY_DIR}/extlib" )
+  SET( qtdir "${basedir}/qt-${qtver}" )
+  SET( qttemp "${qtdir}/qttemp.exe" )
+  FILE( REMOVE "${qttemp}" )
+  IF( NOT EXISTS "${qtdir}/${arch}" )
+
+    MESSAGE( STATUS "Checking availability of qt-${qtver}.${arch} ..." )
+    FILE( DOWNLOAD "${qturl}" "${qttemp}" TIMEOUT 20 INACTIVITY_TIMEOUT 300 STATUS result SHOW_PROGRESS )
+    IF( NOT result EQUAL 0 )
+      MESSAGE( STATUS "qt-${qtver}.${arch} is not available, you may need to manually install Qt on your machine." )
+    ELSE()
+      IF( NOT EXISTS "${qttemp}" )
+        MESSAGE( FATAL_ERROR "Could not download file: ${qturl}" )
+      ELSE()
+        MESSAGE( STATUS "Extracting ..." )
+        EXECUTE_PROCESS( COMMAND "${qttemp}" -y "-o${basedir}" RESULT_VARIABLE sfxresult )
+        IF( NOT sfxresult EQUAL 0 )
+          MESSAGE( FATAL_ERROR "Could not extract qt, error is: ${sfxresult}" )
+        ENDIF()
+      ENDIF()
+    ENDIF()
+    
+  ENDIF()
+
+  SET( qmake_ "${qtdir}/${arch}/bin/qmake" )
+  IF( arch MATCHES "win.*" )
+    SET( qmake_ "${qmake_}.exe" )
+  ENDIF()
+  SET( ${qmake} "${qmake_}" PARENT_SCOPE )  
+
+ENDFUNCTION()
+
+
+FUNCTION( GET_QT_FROM_SERVER qtver )
+
+  IF( WIN32 )
+    IF( CMAKE_SIZEOF_VOID_P EQUAL 4 )
+      SET( arch "win32" )
+    ELSE()
+      SET( arch "win32-amd64" )
+    ENDIF()
+    SET( qtarch "win32" )
+  ELSE()
+    SET( arch ${CMAKE_SYSTEM_NAME} )
+    STRING( TOLOWER ${arch} arch )
+    SET( qtarch ${arch} )
+  ENDIF()
+  
+  IF( COMPILER_IS_GCC_COMPATIBLE )
+    SET( cc "g++" )
+  ELSEIF( MSVC90 )
+    SET( cc "msvc2008" )
+  ELSEIF( MSVC10 )
+    SET( cc "msvc2010" )
+  ENDIF()
+
+  IF( arch AND cc )
+    SET( arch "${arch}-${cc}" )
+    DOWNLOAD_EXTRACT_QT( ${qtver} "${arch}" qmake_ )
+
+    IF( qmake_ AND EXISTS "${qmake_}" )
+      DOWNLOAD_EXTRACT_QT( ${qtver} "include" ignore )
+      SET( ENV{QMAKESPEC} "${qtarch}-${cc}" )
+      UNSET( QT_QMAKE_EXECUTABLE CACHE )
+      SET( QT_QMAKE_EXECUTABLE ${qmake_} CACHE FILEPATH INTERNAL )
+      SET( QT_BCI2000 TRUE CACHE BOOL INTERNAL )
+      MARK_AS_ADVANCED( QT_QMAKE_EXECUTABLE QT_BCI2000 )
+    ENDIF()
+  ENDIF()
+
+ENDFUNCTION()
+
+## Main
 
 IF( USE_EXTERNAL_QT AND QT_BCI2000 )
   UNSET( QT_QMAKE_EXECUTABLE CACHE )
@@ -20,40 +87,19 @@ IF( USE_EXTERNAL_QT AND QT_BCI2000 )
 ENDIF()
 
 IF( NOT USE_EXTERNAL_QT )
-  IF( WIN32 AND CMAKE_SIZEOF_VOID_P EQUAL 4 )
-    SET( qtarch_ "win32" )
-  ENDIF()
-  IF( MINGW )
-    SET( qtcc_ "g++" )
-  ELSEIF( MSVC90 )
-    SET( qtcc_ "msvc2008" )
-  ELSEIF( MSVC10 )
-    SET( qtcc_ "msvc2010" )
-  ENDIF()
-  IF( qtarch_ AND qtcc_ )
-    SET( qtdir_ "${qtarch_}-${qtcc_}" )
-    IF( NOT EXISTS "${BCI2000_SRC_DIR}/extlib/qt/${qtdir_}" )
-      MESSAGE( STATUS "Extracting BCI2000-specific version of Qt (this may take some time)..." )
-      EXECUTE_PROCESS(
-        COMMAND "${BCI2000_SRC_DIR}/extlib/qt.${qtarch_}.exe" -y "-o${BCI2000_SRC_DIR}/extlib"
-        RESULT_VARIABLE sfxresult_
-      )
-      IF( NOT sfxresult_ EQUAL 0 )
-        MESSAGE( FATAL_ERROR "Could not extract qt, error is: ${sfxresult_}" )
-      ENDIF()
-    ENDIF()
-    SET( qmake_ "${BCI2000_SRC_DIR}/extlib/qt/${qtdir_}/bin/qmake.exe" )
-    IF( EXISTS ${qmake_} )
-      SET( ENV{QMAKESPEC} "${qtdir_}" )
-      UNSET( QT_QMAKE_EXECUTABLE CACHE )
-      SET( QT_QMAKE_EXECUTABLE
-        ${BCI2000_SRC_DIR}/extlib/qt/${qtdir_}/bin/qmake.exe
-        CACHE FILEPATH INTERNAL
-      )
-      SET( QT_BCI2000 TRUE CACHE BOOL INTERNAL )
-      MARK_AS_ADVANCED( QT_QMAKE_EXECUTABLE QT_BCI2000 )
-    ENDIF()
-  ENDIF()
+  GET_QT_FROM_SERVER( "4.7.0" )
+ENDIF()
+
+IF( QT_BCI2000 )
+  OPTION( USE_EXTERNAL_QT
+    "Set to ON to dynamically link BCI2000 against an existing Qt installation.
+    Note that this will introduce various run-time dependencies which may be difficult
+    to manage when deploying the resulting executables."
+    OFF
+  )
+ELSE()
+  SET( USE_EXTERNAL_QT ON CACHE BOOL INTERNAL )
+  MARK_AS_ADVANCED( USE_EXTERNAL_QT )
 ENDIF()
 
 FIND_PACKAGE( Qt4 REQUIRED )
