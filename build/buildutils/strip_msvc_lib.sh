ebdc49fce815c8295f6ba5047f401785eed4c118
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2013-06-12 17:41:33 +0000

    Fixing paths in strip_msvc_lib.sh.
    git-svn-id: http://www.bci2000.org/svn/trunk@4489 7ef59cdf-0624-0410-a474-e34ce222aafa

--- build/buildutils/strip_msvc_lib.sh
+++ build/buildutils/strip_msvc_lib.sh
@@ -5,22 +5,30 @@
 #   using GNU binutils.
 # Author: juergen.mellinger@uni-tuebingen.de
 
+dir=$(dirname $0)
+objcopy_=objcopy
+
 function strip_()
 {
   f=$1
   echo Fixing up object file names ...
-  fixup_msvc_lib "$(cygpath -a -w $f)" || exit -1
+  ${dir}/fixup_msvc_lib "$(cygpath -a -w $f)" || exit -1
   echo Clearing .drectve debug flag ...
-  coff_set_debug -v -s.drectve -0 "$(cygpath -a -w $f)" || exit -1
+  ${dir}/coff_set_debug -v -s.drectve -0 "$(cygpath -a -w $f)" || exit -1
   echo Running objcopy -g ...
-  objcopy -g $f || exit -1
+  ${objcopy_} -g $f || exit -1
   echo Enabling .drectve debug flag ...
-  coff_set_debug -v -s.drectve -1 "$(cygpath -a -w $f)" || exit -1
+  ${dir}/coff_set_debug -v -s.drectve -1 "$(cygpath -a -w $f)" || exit -1
 }
 
 if [ ! $1 ]; then
   echo "Expected names of libraries to strip." 2>&1
   exit -1
+fi
+
+if [ "$1"=="-64" ]; then
+  objcopy_=x86_64-pc-cygwin-objcopy
+  shift
 fi
 
 res_=0
