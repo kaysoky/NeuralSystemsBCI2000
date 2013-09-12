f6165360116e142f7649de92b417c41a268f40f2
mellinger <mellinger@7ef59cdf-0624-0410-a474-e34ce222aafa>
2013-09-12 16:03:16 +0000

    Fixed protector define in MicRecorderFilter.h to avoid interference with DataGlove filter.
    git-svn-id: http://www.bci2000.org/svn/trunk@4573 7ef59cdf-0624-0410-a474-e34ce222aafa

--- src/contrib/Application/MicRecorder/MicRecorderFilter.h
+++ src/contrib/Application/MicRecorder/MicRecorderFilter.h
@@ -1,25 +1,25 @@
 /* $BEGIN_BCI2000_LICENSE$
- * 
+ *
  * This file is part of BCI2000, a platform for real-time bio-signal research.
  * [ Copyright (C) 2000-2012: BCI2000 team and many external contributors ]
- * 
+ *
  * BCI2000 is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * BCI2000 is distributed in the hope that it will be useful, but
  *                         WITHOUT ANY WARRANTY
  * - without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * $END_BCI2000_LICENSE$
 /*/
-#ifndef DataGlove5DTUFilterH
-#define DataGlove5DTUFilterH
+#ifndef MicRecorderFilterH
+#define MicRecorderFilterH
 
 #include "GenericFilter.h"
 #include "MicRecorder.h"
@@ -36,11 +36,11 @@ class MicRecorderFilter : public GenericFilter
 
  private:
 
-	 MicRecorder mMic;
-	 bool mEnable;
+   MicRecorder mMic;
+   bool mEnable;
 };
 
-#endif // DataGlove5DTUFilterH
+#endif // MicRecorderFilterH
 
 
 
