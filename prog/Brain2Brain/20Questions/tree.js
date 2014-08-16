ef47ac41b416e05ae65264f9cce567b4cf4de98f
Joseph Wu <kaysoky88@gmail.com>
2014-08-16 11:43:51 -0700

    Change 20 questions game to build a tree using D3.  The implementation is functional, but could be further fleshed out.
    Conflicts:
    
    	prog/Brain2Brain/Constants.js

--- prog/Brain2Brain/20Questions/tree.js
+++ prog/Brain2Brain/20Questions/tree.js
@@ -1,4 +1,8 @@
 var QuestionTree = {};
+var LastSelectedNode = null;
+
+var NodeWidth = 150;
+var NodeHeight = 75;
 
 /**
  * Fetches the file from the server and parses it into a question/answer tree
@@ -9,8 +13,78 @@ InitializeQuestionTree = function(filename) {
     .done(function(data) {
         var lines = data.split('\n');
 
-        // Build the tree
+        // Build the tree in memory
         QuestionTree = BuildQuestionTree(lines);
+        
+        // Prepare the SVG for tree-ification
+        var TreeWidth = GetAnswers(QuestionTree).length * NodeWidth;
+        var TreeHeight = GetDepth(QuestionTree) * NodeHeight;
+        var vis = d3.select('#QuestionTree').append('svg')
+            .attr('width', TreeWidth + NodeWidth).attr('height', TreeHeight + NodeHeight)
+            .attr('transform', 'translate(' + (NodeWidth / 2) + ',' + (NodeHeight / 2) + ')');
+
+        // Initialize the D3 tree layout
+        var tree = d3.layout.tree()
+            .size([TreeWidth, TreeHeight])
+            .children(function (d) {
+                return d.question ? [d.left, d.right] : null;
+            });
+
+        // Convert the data into nodes and links
+        var nodes = tree.nodes(QuestionTree);
+        var links = tree.links(nodes);
+   
+        // Initialize the links
+        var diagonal = d3.svg.diagonal();
+        var link = vis.selectAll('path')
+            .data(links)
+            .enter().append('path')
+            .attr('class', 'link')
+            .attr('d', diagonal)
+            .attr('id', function (d) {
+                return 'Link-' + d.source.id + '-' + d.target.id;
+            });
+
+        // Initialize the nodes
+        var node = vis.selectAll('g.node')
+            .data(nodes)
+            .enter().append('g')
+            .attr('id', function (d) { return 'Node-' + d.id; })
+            .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; })
+            .on('mouseover', function () {
+                d3.select(this).select('foreignObject > div').classed({ 'highlighted': true });
+            })
+            .on('mouseout', function () {
+                d3.select(this).select('foreignObject > div').classed({ 'highlighted': false });
+            })
+            .on('click', function (d) {
+                var element = d3.select(this);
+                
+                // Submit the question when clicking the node for the first time
+                if (!element.classed('explored')) {
+                    LastSelectedNode = d;
+                    SubmitQuestion(d.question || d.answer, d.question);
+                    element.classed({ 'explored': true });
+                    d3.select('#Link-' + d.parent.id + '-' + d.id).classed({ 'explored': true });
+                }
+            });
+
+        // Add the dot at every node
+        node.append('circle').attr('r', NodeWidth / 10);
+
+        // place the name atribute left or right depending if children
+        node.append('foreignObject')
+            .attr('transform', 'translate(-' + (NodeWidth / 2) + ', -' + (NodeWidth / 15) + ')')
+            .attr('width', NodeWidth)
+            .attr('height', NodeHeight)
+            .append("xhtml:div")
+            .style({ 'width': NodeWidth + 'px',
+                'height': NodeHeight + 'px' })
+            .text(function(d) { return d.question || d.answer; });
+
+        $('svg').draggable().css({
+            'left': ($('#QuestionTree').width() / 2 - (TreeWidth + NodeWidth) / 2) + 'px'
+        });
 
         // Hide the scope of the next operation
         // Since the client determines the answer to the game that is being played,
@@ -42,9 +116,12 @@ InitializeQuestionTree = function(filename) {
     });
 };
 
-AnswerQuestion = function(content, isYes) {
-    QuestionTree = PruneTree(QuestionTree, content, isYes ? 'left' : 'right');
-    UpdateSelectors();
+AnswerQuestion = function(isYes) {
+    if (LastSelectedNode && LastSelectedNode.children && LastSelectedNode.children.length === 2) {
+        var LinkPrefix = '#Link-' + LastSelectedNode.id + '-';
+        d3.select(LinkPrefix + LastSelectedNode.children[0].id).classed({ 'selected': isYes });
+        d3.select(LinkPrefix + LastSelectedNode.children[1].id).classed({ 'selected': !isYes });
+    }
 }
 
 /******************************************************************************
@@ -52,94 +129,6 @@ AnswerQuestion = function(content, isYes) {
  ******************************************************************************/
 // Note: there is a way to make these functions actually private (anonymous functions),
 //       but the added complexity would be unnecessary
-
-/**
- * Searches the tree for the given node ("content")
- *   and prunes the branch
- *   Any questions in "content" will remain on the tree
- *   Any answers in "content" will be removed from the tree
- * Parameter rightOrLeft is required when pruning questions
- *   and unnecessary/ignored when pruning answers
- *   The parameter should be "left" if a question is answered with YES
- *     and "right" if a question is answered with NO
- */
-PruneTree = function(tree, content, rightOrLeft) {
-    // Argument check!
-    if (rightOrLeft) {
-        if (rightOrLeft != 'right' && rightOrLeft != 'left') {
-            alert('Invalid value for parameter "rightOrLeft": ' + rightOrLeft);
-        }
-    }
-
-    // Is this a question node?
-    if (tree.question) {
-        // Found the node to prune
-        if (tree.question == content) {
-            // A question was asked, so keep the specified branch
-            return tree[rightOrLeft];
-
-        // An answer was rejected, so keep the other branch
-        } else if (tree.left == content) {
-            return tree.right;
-        } else if (tree.right == content) {
-            return tree.left;
-
-        } else {
-            // Keep traversing
-            tree.left = PruneTree(tree.left, content, rightOrLeft);
-            tree.right = PruneTree(tree.right, content, rightOrLeft);
-        }
-    
-    // Edge case: only one answer remains
-    } else if (tree == content) {
-        alert("Game over :(\nClose this alert to refresh the page");
-        location.reload();
-    }
-
-    return tree;
-};
-
-/**
- * Fills in the Questions/Answers selectables with the current tree
- * Returns a random element from the array of possible answers
- */
-UpdateSelectors = function() {
-    // Clear the existing items
-    $('#QuestionsList, #AnswersList').empty();
-
-    // Fill in the questions
-    var questions = GetQuestions(QuestionTree);
-    var qList = $('#QuestionsList');
-    for (var i in questions) {
-        if (i >= MAX_SELECTABLES) {
-            break;
-        }
-        qList.append('<li class="ui-widget-content">' + questions[i] + '</li>')
-    }
-
-    // Fill in the answers
-    var answers = GetAnswers(QuestionTree);
-    var aList = $('#AnswersList');
-    for (var i in answers) {
-        if (i >= MAX_SELECTABLES) {
-            break;
-        }
-        aList.append('<li class="ui-widget-content">' + answers[i] + '</li>')
-    }
-
-    // Make the lists pretty and interactive
-    $('#QuestionsList, #AnswersList').selectable({
-        stop: function(event, ui) {
-            // Prevent multi-selection within a single selectable
-            $(event.target).children('.ui-selected').not(':first').removeClass('ui-selected');
-
-            // Prevent multi-selection between the two selectables
-            $('#QuestionsList, #AnswersList').not(event.target).children('.ui-selected').removeClass('ui-selected');
-        }
-    });
-
-    return answers[Math.floor(Math.random() * answers.length)];
-};
 
 /**
  * Builds up the question tree
@@ -159,6 +148,7 @@ UpdateSelectors = function() {
  *      /           \
  *  Bird            Cat
  */
+var NodeIDCounter = 0;
 BuildQuestionTree = function(elements) {
     // Finished parsing
     if (elements.length <= 0) {
@@ -169,10 +159,12 @@ BuildQuestionTree = function(elements) {
     var line = elements.shift();
     var parts = line.split(':');
 
+    var node = {};
+    node.id = NodeIDCounter++;
     if (parts[0] == 'A') {
-        return parts[1].trim();
+        node.answer = parts[1].trim();
+        return node;
     } else if (parts[0] == 'Q') {
-        var node = {};
         node.question = parts[1].trim();
         node.left = BuildQuestionTree(elements);
         node.right = BuildQuestionTree(elements);
@@ -180,23 +172,6 @@ BuildQuestionTree = function(elements) {
     } else {
         LogInfo('Could not parse line ' + index + ': ' + line);
     }
-};
-
-/**
- * Returns a list of all the questions contained in the tree
- * Traversal order is: middle -> left -> right
- */
-GetQuestions = function(tree) {
-    var questions = [];
-
-    // Is this a question node?
-    if (tree.question) {
-        return [tree.question]
-            .concat(GetQuestions(tree.left))
-            .concat(GetQuestions(tree.right));
-    }
-
-    return [];
 };
 
 /**
@@ -212,4 +187,12 @@ GetAnswers = function(tree) {
     }
 
     return [tree];
+};
+
+GetDepth = function (tree) {
+    if (tree.question) {
+        return Math.max(GetDepth(tree.left), GetDepth(tree.right)) + 1;
+    }
+
+    return 1;
 };
