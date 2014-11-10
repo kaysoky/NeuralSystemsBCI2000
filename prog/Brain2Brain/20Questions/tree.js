684c57ce24d51fdbb37f78f21d60ac20455ef1ee
DLos <loseydm45@gmail.com>
2014-11-09 19:03:21 -0800

    Added file name to AppLog

--- prog/Brain2Brain/20Questions/tree.js
+++ prog/Brain2Brain/20Questions/tree.js
@@ -25,8 +25,8 @@ InitializeQuestionMatrix = function(filename) {
             TheAnswer = UpdateSelectors();
 
             // Tell the BCI-side what the client is playing for
-            LogInfo('\n The file being used is: ' + filename + "\n" + 
-                    'Answer is ' + TheAnswer);
+            LogInfo('The file being used is: ' + filename);
+            LogInfo('Answer is ' + TheAnswer);
             PUT_TextAnswer(TheAnswer, function () {
                 alert('Application not ready to begin');
                 location.reload();
