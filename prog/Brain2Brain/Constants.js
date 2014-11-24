a9580d99b9e122e5e080c3a51cf09898709e0a9e
DLos <loseydm45@gmail.com>
2014-11-23 18:45:43 -0800

    Attempt 2

--- prog/Brain2Brain/Constants.js
+++ prog/Brain2Brain/Constants.js
@@ -1,4 +1,14 @@
-/*
- * How long each trial should take; needs to match feedbackduration in BCI2000
+/**
+ * How frequently should the game poll the server for a status update?
  */
-var TRIAL_TIME = 9000.0; // Milliseconds
+var POLL_INTERVAL = 250; // Milliseconds
+
+/**
+ * How long should the game wait between seeing the "Run Ended" signal and refreshing the page?
+ */
+var REFRESH_WAIT = 5000; // Milliseconds
+
+/**
+ * TMS port
+ */
+var TMS_PORT = 25000;
